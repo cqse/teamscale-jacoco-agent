ad473f1b71661804ce99266abdd520668b26442e
Fabian Streitel <streitel@cqse.eu>
2017-08-01 16:46:00 +0200

    figured out how to correctly read from socket

--- eu.cqse.teamscale.jacoco.converter/src/main/java/eu/cqse/teamscale/jacoco/converter/JacocoRemoteTCPController.java
+++ eu.cqse.teamscale.jacoco.converter/src/main/java/eu/cqse/teamscale/jacoco/converter/JacocoRemoteTCPController.java
@@ -5,6 +5,8 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Date;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.conqat.lib.commons.assertion.CCSMAssert;
 import org.jacoco.core.data.ExecutionData;
 import org.jacoco.core.data.ExecutionDataStore;
@@ -53,12 +55,32 @@ public class JacocoRemoteTCPController implements IJacocoController {
 
 	/** {@inheritDoc} */
 	@Override
+	// TODO (FS) returned observable should be observed on background thread so we
+	// don't block the reader thread
+	// TODO (FS) thus, observe on single later on for synchronization of dumps
+	// TODO (FS) clean up background read thread etc with observables
 	public synchronized Observable<Dump> connect() throws IOException {
 		socket = new Socket(InetAddress.getByName(address), port);
 		writer = new RemoteControlWriter(socket.getOutputStream());
 		reader = new RemoteControlReader(socket.getInputStream());
 		reader.setSessionInfoVisitor(info -> lastReceivedSession = info);
 		reader.setExecutionDataVisitor(this::storeExecutionData);
+
+		new Thread(() -> {
+			Logger logger = LogManager.getLogger(this);
+			try {
+				logger.info("starting read loop");
+				while (reader.read()) {
+					logger.info("wait for next read");
+					subject.onNext(new Dump(lastReceivedSession, store));
+					store = new ExecutionDataStore();
+				}
+			} catch (IOException e) {
+				logger.error("failed to read", e);
+			}
+			logger.info("stream ended");
+		}).start();
+
 		return subject;
 	}
 
@@ -77,10 +99,6 @@ public class JacocoRemoteTCPController implements IJacocoController {
 	public synchronized void dump(boolean reset) throws IOException {
 		CCSMAssert.isTrue(isConnected(), "You must connect before you can start issuing dump commands");
 		writer.visitDumpCommand(true, reset);
-		// must read afterwards or data will not be received
-		reader.read();
-		subject.onNext(new Dump(lastReceivedSession, store));
-		store = new ExecutionDataStore();
 	}
 
 	/** {@inheritDoc} */
