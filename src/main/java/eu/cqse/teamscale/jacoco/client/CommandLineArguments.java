45abfaf1fe37feaf8b130a63c609bc98b15440c4
Fabian Streitel <streitel@cqse.eu>
2017-10-23 14:45:25 +0200

    extract data object for all command line options for readability

---
+++ src/main/java/eu/cqse/teamscale/jacoco/client/CommandLineArguments.java
@@ -0,0 +1,169 @@
+/*-------------------------------------------------------------------------+
+|                                                                          |
+| Copyright (c) 2009-2017 CQSE GmbH                                        |
+|                                                                          |
++-------------------------------------------------------------------------*/
+package eu.cqse.teamscale.jacoco.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.conqat.lib.commons.assertion.CCSMAssert;
+import org.conqat.lib.commons.filesystem.FileSystemUtils;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+/**
+ * Encapsulates all command line options for parsing with {@link JCommander}.
+ */
+public class CommandLineArguments {
+
+	/** The directories and/or zips that contain all class files being profiled. */
+	@Parameter(names = { "--classDir", "--jar", "-c" }, required = true, description = ""
+			+ "The directories or zip/ear/jar/war/... files that contain the compiled Java classes being profiled."
+			+ " Searches recursively, including inside zips.")
+	private List<String> classDirectoriesOrZips = new ArrayList<>();
+
+	/**
+	 * Ant-style include patterns to apply during JaCoCo's traversal of class files.
+	 */
+	@Parameter(names = { "--filter", "-f" }, description = ""
+			+ "Ant-style include patterns to apply to all found class file locations during JaCoCo's traversal of class files."
+			+ " Note that zip contents are separated from zip files with @ and that you can filter only"
+			+ " class files, not intermediate folders/zips. Use with great care as missing class files"
+			+ " lead to broken coverage files! Turn on debug logging to see which locations are being filtered."
+			+ " Defaults to no filtering. Excludes overrule includes.")
+	private List<String> locationIncludeFilters = new ArrayList<>();
+
+	/**
+	 * Ant-style exclude patterns to apply during JaCoCo's traversal of class files.
+	 */
+	@Parameter(names = { "--exclude", "-e" }, description = ""
+			+ "Ant-style exclude patterns to apply to all found class file locations during JaCoCo's traversal of class files."
+			+ " Note that zip contents are separated from zip files with @ and that you can filter only"
+			+ " class files, not intermediate folders/zips. Use with great care as missing class files"
+			+ " lead to broken coverage files! Turn on debug logging to see which locations are being filtered."
+			+ " Defaults to no filtering. Excludes overrule includes.")
+	private List<String> locationExcludeFilters = new ArrayList<>();
+
+	/** The JaCoCo port. */
+	@Parameter(names = { "--port", "-p" }, required = true, description = ""
+			+ "The port under which JaCoCo is listening for connections.")
+	private int port = 0;
+
+	/** The directory to write the XML traces to. */
+	@Parameter(names = { "--out", "-o" }, required = true, description = ""
+			+ "The directory to write the generated XML reports to.")
+	private String outputDir = "";
+
+	/** The interval in minutes for dumping XML data. */
+	@Parameter(names = { "--interval", "-i" }, required = true, description = ""
+			+ "Interval in minutes after which the current coverage is retrived and stored in a new XML file.")
+	private int dumpIntervalInMinutes = 0;
+
+	/** The interval in seconds for a reconnect to the application. */
+	@Parameter(names = { "--reconnect", "-r" }, required = false, description = ""
+			+ "Interval in seconds after which to try and reconnect after losing connection to JaCoCo.")
+	private int reconnectIntervalInSeconds = 5 * 60;
+
+	/** Whether to ignore duplicate, non-identical class files. */
+	@Parameter(names = { "--ignore-duplicates", "-d" }, required = false, description = ""
+			+ "Whether to ignore duplicate, non-identical class files."
+			+ " This is discouraged and may result in incorrect coverage files. Defaults to false.")
+	private boolean shouldIgnoreDuplicateClassFiles = false;
+
+	/** @see #classDirectoriesOrZips */
+	public List<String> getClassDirectoriesOrZips() {
+		return classDirectoriesOrZips;
+	}
+
+	/** @see #classDirectoriesOrZips */
+	public void setClassDirectoriesOrZips(List<String> classDirectoriesOrZips) {
+		this.classDirectoriesOrZips = classDirectoriesOrZips;
+	}
+
+	/** @see #locationIncludeFilters */
+	public List<String> getLocationIncludeFilters() {
+		return locationIncludeFilters;
+	}
+
+	/** @see #locationIncludeFilters */
+	public void setLocationIncludeFilters(List<String> locationIncludeFilters) {
+		this.locationIncludeFilters = locationIncludeFilters;
+	}
+
+	/** @see #locationExcludeFilters */
+	public List<String> getLocationExcludeFilters() {
+		return locationExcludeFilters;
+	}
+
+	/** @see #locationExcludeFilters */
+	public void setLocationExcludeFilters(List<String> locationExcludeFilters) {
+		this.locationExcludeFilters = locationExcludeFilters;
+	}
+
+	/** @see #port */
+	public int getPort() {
+		return port;
+	}
+
+	/** @see #port */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/** @see #outputDir */
+	public String getOutputDir() {
+		return outputDir;
+	}
+
+	/** @see #outputDir */
+	public void setOutputDir(String outputDir) {
+		this.outputDir = outputDir;
+	}
+
+	/** @see #dumpIntervalInMinutes */
+	public int getDumpIntervalInMinutes() {
+		return dumpIntervalInMinutes;
+	}
+
+	/** @see #dumpIntervalInMinutes */
+	public void setDumpIntervalInMinutes(int dumpIntervalInMinutes) {
+		this.dumpIntervalInMinutes = dumpIntervalInMinutes;
+	}
+
+	/** @see #reconnectIntervalInSeconds */
+	public int getReconnectIntervalInSeconds() {
+		return reconnectIntervalInSeconds;
+	}
+
+	/** @see #reconnectIntervalInSeconds */
+	public void setReconnectIntervalInSeconds(int reconnectIntervalInSeconds) {
+		this.reconnectIntervalInSeconds = reconnectIntervalInSeconds;
+	}
+
+	/** @see #shouldIgnoreDuplicateClassFiles */
+	public boolean isShouldIgnoreDuplicateClassFiles() {
+		return shouldIgnoreDuplicateClassFiles;
+	}
+
+	/** @see #shouldIgnoreDuplicateClassFiles */
+	public void setShouldIgnoreDuplicateClassFiles(boolean shouldIgnoreDuplicateClassFiles) {
+		this.shouldIgnoreDuplicateClassFiles = shouldIgnoreDuplicateClassFiles;
+	}
+
+	/** Makes sure the arguments are valid. */
+	public void validate() throws IOException {
+		for (String path : getClassDirectoriesOrZips()) {
+			CCSMAssert.isTrue(new File(path).exists(), "Path '" + path + "' does not exist");
+			CCSMAssert.isTrue(new File(path).canRead(), "Path '" + path + "' is not readable");
+		}
+
+		FileSystemUtils.ensureDirectoryExists(new File(getOutputDir()));
+		CCSMAssert.isTrue(new File(getOutputDir()).canWrite(), "Path '" + getOutputDir() + "' is not writable");
+	}
+
+}
--- src/main/java/eu/cqse/teamscale/jacoco/client/Main.java
+++ src/main/java/eu/cqse/teamscale/jacoco/client/Main.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
@@ -17,14 +15,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
-import org.conqat.lib.commons.assertion.CCSMAssert;
 import org.conqat.lib.commons.collections.CollectionUtils;
-import org.conqat.lib.commons.filesystem.FileSystemUtils;
 import org.conqat.lib.commons.string.StringUtils;
 import org.jacoco.core.JaCoCo;
 
 import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 
 import io.reactivex.schedulers.Schedulers;
@@ -50,59 +45,8 @@ public class Main {
 	 */
 	private static final Marker RECONNECT_LOGGER_MARKER = MarkerManager.getMarker("RECONNECT");
 
-	/** The directories and/or zips that contain all class files being profiled. */
-	@Parameter(names = { "--classDir", "--jar", "-c" }, required = true, description = ""
-			+ "The directories or zip/ear/jar/war/... files that contain the compiled Java classes being profiled."
-			+ " Searches recursively, including inside zips.")
-	private List<String> classDirectoriesOrZips = new ArrayList<>();
-
-	/**
-	 * Ant-style include patterns to apply during JaCoCo's traversal of class files.
-	 */
-	@Parameter(names = { "--filter", "-f" }, description = ""
-			+ "Ant-style include patterns to apply to all found class file locations during JaCoCo's traversal of class files."
-			+ " Note that zip contents are separated from zip files with @ and that you can filter only"
-			+ " class files, not intermediate folders/zips. Use with great care as missing class files"
-			+ " lead to broken coverage files! Turn on debug logging to see which locations are being filtered."
-			+ " Defaults to no filtering. Excludes overrule includes.")
-	private List<String> locationIncludeFilters = new ArrayList<>();
-
-	/**
-	 * Ant-style exclude patterns to apply during JaCoCo's traversal of class files.
-	 */
-	@Parameter(names = { "--exclude", "-e" }, description = ""
-			+ "Ant-style exclude patterns to apply to all found class file locations during JaCoCo's traversal of class files."
-			+ " Note that zip contents are separated from zip files with @ and that you can filter only"
-			+ " class files, not intermediate folders/zips. Use with great care as missing class files"
-			+ " lead to broken coverage files! Turn on debug logging to see which locations are being filtered."
-			+ " Defaults to no filtering. Excludes overrule includes.")
-	private List<String> locationExcludeFilters = new ArrayList<>();
-
-	/** The JaCoCo port. */
-	@Parameter(names = { "--port", "-p" }, required = true, description = ""
-			+ "The port under which JaCoCo is listening for connections.")
-	private int port = 0;
-
-	/** The directory to write the XML traces to. */
-	@Parameter(names = { "--out", "-o" }, required = true, description = ""
-			+ "The directory to write the generated XML reports to.")
-	private String outputDir = "";
-
-	/** The interval in minutes for dumping XML data. */
-	@Parameter(names = { "--interval", "-i" }, required = true, description = ""
-			+ "Interval in minutes after which the current coverage is retrived and stored in a new XML file.")
-	private int dumpIntervalInMinutes = 0;
-
-	/** The interval in seconds for a reconnect to the application. */
-	@Parameter(names = { "--reconnect", "-r" }, required = false, description = ""
-			+ "Interval in seconds after which to try and reconnect after losing connection to JaCoCo.")
-	private int reconnectIntervalInSeconds = 5 * 60;
-
-	/** Whether to ignore duplicate, non-identical class files. */
-	@Parameter(names = { "--ignore-duplicates", "-d" }, required = false, description = ""
-			+ "Whether to ignore duplicate, non-identical class files."
-			+ " This is discouraged and may result in incorrect coverage files. Defaults to false.")
-	private boolean shouldIgnoreDuplicateClassFiles = false;
+	/** The parsed command line arguments */
+	private CommandLineArguments arguments = new CommandLineArguments();
 
 	/** The logger. */
 	private final Logger logger = LogManager.getLogger(this);
@@ -123,7 +67,16 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Main main = new Main();
 
-		JCommander jCommander = JCommander.newBuilder().programName(Main.class.getName()).addObject(main).build();
+		main.parseCommandLine(args);
+		main.loop();
+	}
+
+	/**
+	 * Parses the given command line arguments. Exits the program or throws an
+	 * exception if the arguments are not valid.
+	 */
+	private void parseCommandLine(String[] args) throws IOException {
+		JCommander jCommander = JCommander.newBuilder().programName(Main.class.getName()).addObject(arguments).build();
 		try {
 			jCommander.parse(args);
 		} catch (ParameterException e) {
@@ -132,24 +85,12 @@ public class Main {
 			System.exit(1);
 		}
 
-		main.validate();
-		main.loop();
+		arguments.validate();
 	}
 
 	/** Constructor. */
 	public Main() {
 		logger.info("Starting JaCoCo client " + VERSION + " compiled against JaCoCo " + JaCoCo.VERSION);
-	}
-
-	/** Makes sure the arguments are valid. */
-	private void validate() throws IOException {
-		for (String path : classDirectoriesOrZips) {
-			CCSMAssert.isTrue(new File(path).exists(), "Path '" + path + "' does not exist");
-			CCSMAssert.isTrue(new File(path).canRead(), "Path '" + path + "' is not readable");
-		}
-
-		FileSystemUtils.ensureDirectoryExists(new File(outputDir));
-		CCSMAssert.isTrue(new File(outputDir).canWrite(), "Path '" + outputDir + "' is not writable");
 	}
 
 	/**
@@ -158,22 +99,24 @@ public class Main {
 	 * 
 	 * Handles the following error cases:
 	 * <ul>
-	 * <li>Application is not running: wait for {@link #reconnectIntervalInSeconds}
-	 * then retry
+	 * <li>Application is not running: wait for
+	 * {@link CommandLineArguments#getReconnectIntervalInSeconds()} then retry
 	 * <li>Fatal error: immediately restart
 	 * </ul>
 	 */
 	private void loop() {
-		logger.info("Connecting to JaCoCo on localhost:{} and dumping coverage every {} minutes to {}", port,
-				dumpIntervalInMinutes, outputDir);
-		if (!locationIncludeFilters.isEmpty() || !locationExcludeFilters.isEmpty()) {
+		logger.info("Connecting to JaCoCo on localhost:{} and dumping coverage every {} minutes to {}",
+				arguments.getPort(), arguments.getDumpIntervalInMinutes(), arguments.getOutputDir());
+		if (!arguments.getLocationIncludeFilters().isEmpty() || !arguments.getLocationExcludeFilters().isEmpty()) {
 			logger.warn("Class file filters are enabled. Includes: {}, excludes: {}",
-					StringUtils.concat(locationIncludeFilters, ", "), StringUtils.concat(locationExcludeFilters, ", "));
+					StringUtils.concat(arguments.getLocationIncludeFilters(), ", "),
+					StringUtils.concat(arguments.getLocationExcludeFilters(), ", "));
 		}
 
-		converter = new XmlReportGenerator(CollectionUtils.map(classDirectoriesOrZips, File::new),
-				locationIncludeFilters, locationExcludeFilters, shouldIgnoreDuplicateClassFiles);
-		store = new TimestampedFileStore(Paths.get(outputDir));
+		converter = new XmlReportGenerator(CollectionUtils.map(arguments.getClassDirectoriesOrZips(), File::new),
+				arguments.getLocationIncludeFilters(), arguments.getLocationExcludeFilters(),
+				arguments.isShouldIgnoreDuplicateClassFiles());
+		store = new TimestampedFileStore(Paths.get(arguments.getOutputDir()));
 
 		while (true) {
 			try {
@@ -182,9 +125,9 @@ public class Main {
 				logger.error(RECONNECT_LOGGER_MARKER,
 						"Could not connect to JaCoCo. The application appears not to be running."
 								+ " Trying to reconnect in {} seconds",
-						reconnectIntervalInSeconds, e);
+						arguments.getReconnectIntervalInSeconds(), e);
 				try {
-					Thread.sleep(TimeUnit.SECONDS.toMillis(reconnectIntervalInSeconds));
+					Thread.sleep(TimeUnit.SECONDS.toMillis(arguments.getReconnectIntervalInSeconds()));
 				} catch (InterruptedException e2) {
 					// ignore, retry early
 				}
@@ -200,7 +143,7 @@ public class Main {
 	 * exception occurs.
 	 */
 	private void run() throws IOException, InterruptedException, ExecutionException {
-		try (IJacocoController controller = new JacocoRemoteTCPController("localhost", port)) {
+		try (IJacocoController controller = new JacocoRemoteTCPController("localhost", arguments.getPort())) {
 			controller.connect().doOnNext(data -> {
 				logger.info("Received dump, converting");
 			}).map(converter::convert).doOnNext(data -> {
@@ -238,7 +181,7 @@ public class Main {
 						+ " the application was interrupted. Will try to reconnect", e);
 				restart();
 			}
-		}, dumpIntervalInMinutes, dumpIntervalInMinutes, TimeUnit.MINUTES);
+		}, arguments.getDumpIntervalInMinutes(), arguments.getDumpIntervalInMinutes(), TimeUnit.MINUTES);
 	}
 
 	/**
