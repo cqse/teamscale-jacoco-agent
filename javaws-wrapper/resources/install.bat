d7e94f7919f053fe4990db00783b752ddda2a435
Fabian Streitel <streitel@cqse.eu>
2018-05-08 16:36:38 +0200

    nicer messages during installation [skip ci]

--- javaws-wrapper/resources/install.bat
+++ javaws-wrapper/resources/install.bat
@@ -1,2 +1,5 @@
 ftype JNLPFile > JNLPFile.ftype.bak
 ftype JNLPFile=%~dp0\javaws.exe
+
+echo Installed. Please restart your web browser if it is still running!
+echo Run uninstall.bat to restore the old behaviour without a profiler.
--- javaws-wrapper/resources/uninstall.bat
+++ javaws-wrapper/resources/uninstall.bat
@@ -1,6 +1,8 @@
 if exist <insert file name here> (
     set /p oldftype=<JNLPFile.ftype.bak
 	ftype %oldftype%
+	
+	echo Uninstalled.
 ) else (
-    echo No backup file found, cannot restore old file association. You can restore it manually or by reinstalling Java
+    echo No backup file found, cannot restore old file association. You can restore it manually or by reinstalling Java.
 )
