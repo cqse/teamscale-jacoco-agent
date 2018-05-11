cec37e52f46518abc77c5708d729a5d0d3abf703
Fabian Streitel <streitel@cqse.eu>
2018-05-11 10:21:59 +0200

    fixing some quirks in install scripts

--- javaws-wrapper/src/dist/install.bat
+++ javaws-wrapper/src/dist/install.bat
@@ -1,8 +1,14 @@
 @echo off
-echo Installation will fail unless you have Administrator rights!
+
+if exist JNLPFile.ftype.bak (
+	echo Already installed.
+	exit
+)
 
 ftype JNLPFile > JNLPFile.ftype.bak
-ftype JNLPFile=%~dp0\bin\javaws "%%1"
+ftype JNLPFile=%~dp0\javaws.exe "%%1"
 
-echo Installed. Please restart your web browser if it is still running!
+echo Installed. Changed to:
+ftype JNLPFile
 echo Run uninstall.bat to restore the old behaviour without a profiler.
+
--- javaws-wrapper/src/dist/uninstall.bat
+++ javaws-wrapper/src/dist/uninstall.bat
@@ -1,11 +1,10 @@
 @echo off
-echo Uninstallation will fail unless you have Administrator rights!
-
 if exist JNLPFile.ftype.bak (
     set /p oldftype=<JNLPFile.ftype.bak
 	ftype %oldftype%
-	
-	echo Uninstalled.
+	echo Uninstalled. Reverted back to:
+	ftype JNLPFile
 ) else (
     echo No backup file found, cannot restore old file association. You can restore it manually or by reinstalling Java.
 )
+
