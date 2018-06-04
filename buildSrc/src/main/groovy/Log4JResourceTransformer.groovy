923dc81b4003ee939e499806f9161c6e7bbe3e28
Fabian Streitel <streitel@cqse.eu>
2018-06-04 17:31:28 +0200

    rework

--- buildSrc/src/main/groovy/Log4JResourceTransformer.groovy
+++ buildSrc/src/main/groovy/Log4JResourceTransformer.groovy
@@ -9,59 +9,64 @@ import java.io.InputStream
 
 /**
  * Relocates the log4j-provider.properties file.
- * This is currently not handled by the log4j relocation plugin.
+ *
+ * This files contains a reference to a Log4J class that is read
+ * at runtime. Since we use the shadow plugin during the build to relocate
+ * all of our dependencies to different packages, we need to update this
+ * class name to the relocated class name.
+ * This is currently not handled by the log4j relocation plugin we use.
  */
 public class Log4JResourceTransformer implements Transformer {
 
-    private static final String PROPERTY_FILE = "log4j-provider.properties"
+	private static final String PROPERTY_FILE = "log4j-provider.properties"
 
-    private List<String> lines = null
-    private String path = null
+	private List<String> lines = null
+	private String path = null
 
-    boolean canTransformResource(FileTreeElement element) {
+	boolean canTransformResource(FileTreeElement element) {
 		return element.relativePath.lastName.equalsIgnoreCase(PROPERTY_FILE)
-    }
+	}
 
-    void transform(TransformerContext context) {
-    	path = context.path
-        lines = context.is.readLines()
-        lines = lines.collect { relocate(it, context) }
-        context.is.close()
-    }
+	void transform(TransformerContext context) {
+		path = context.path
+		lines = context.is.readLines()
+		lines = lines.collect { relocate(it, context) }
+		context.is.close()
+	}
 
-    boolean hasTransformedResource() {
-        return lines != null
-    }
+	boolean hasTransformedResource() {
+		return lines != null
+	}
 
-    void modifyOutputStream(ZipOutputStream os) {
-        os.putNextEntry(new ZipEntry(path))
-        def writer = os.newPrintWriter()
-        lines.each {
-        	writer.println(it)
-        }
-        writer.flush()
-        lines = null
-    }
+	void modifyOutputStream(ZipOutputStream os) {
+		os.putNextEntry(new ZipEntry(path))
+		def writer = os.newPrintWriter()
+		lines.each {
+			writer.println(it)
+		}
+		writer.flush()
+		lines = null
+	}
 
-    private String relocate(String line, TransformerContext context) {
-        def matcher = line =~ /^(.*)(org.apache.*)$/
-        if (!matcher) {
-            return line
-        }
+	private String relocate(String line, TransformerContext context) {
+		def matcher = line =~ /^(.*)(org.apache.*)$/
+		if (!matcher) {
+			return line
+		}
 
-        def prefix = matcher.group(1)
-        def className = matcher.group(2)
-        return prefix + relocateClassName(className, context)
-    }
+		def prefix = matcher.group(1)
+		def className = matcher.group(2)
+		return prefix + relocateClassName(className, context)
+	}
 
-    private String relocateClassName(String className, TransformerContext context) {
-        def relocatorContext = RelocateClassContext.builder().className(className).stats(context.stats).build()
-        for (def relocator : context.relocators) {
-            if (relocator.canRelocateClass(relocatorContext)) {
-                return relocator.relocateClass(relocatorContext)
-            }
-        }
-        return className
-    }
+	private String relocateClassName(String className, TransformerContext context) {
+		def relocatorContext = RelocateClassContext.builder().className(className).stats(context.stats).build()
+		for (def relocator : context.relocators) {
+			if (relocator.canRelocateClass(relocatorContext)) {
+				return relocator.relocateClass(relocatorContext)
+			}
+		}
+		return className
+	}
 }
 
