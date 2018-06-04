3c0b856219617ae6a69f7bf779f3b3aaecfb6bfb
Fabian Streitel <streitel@cqse.eu>
2018-06-04 17:33:09 +0200

    rework

--- src/main/java/eu/cqse/teamscale/jacoco/client/report/DuplicateIgnoringCoverageBuilder.java
+++ src/main/java/eu/cqse/teamscale/jacoco/client/report/DuplicateIgnoringCoverageBuilder.java
@@ -27,7 +27,7 @@ import org.jacoco.core.analysis.IClassCoverage;
 			logger.warn("Ignoring duplicate, non-identical class file for class {} compiled from source file {}."
 					+ " This happens when a class with the same fully-qualified name is loaded twice but the two loaded class files are not identical."
 					+ " A common reason for this is that the same library or shared code is included twice in your application but in two different versions."
-					+ " The produced coverage for this class may not be accurate or may even unusable."
+					+ " The produced coverage for this class may not be accurate or may even be unusable."
 					+ " To fix this problem, please resolve the conflict between both class files in your application.",
 					coverage.getName(), coverage.getSourceFileName(), e);
 		}
