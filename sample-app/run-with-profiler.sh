80a4fea6782689e393f15f9ae578977b7619d58b
Fabian Streitel <streitel@cqse.eu>
2018-09-22 21:41:28 +0200

    add simple script to start sample app with agent

---
+++ sample-app/run-with-profiler.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+profiler_dist="../agent/build/distributions/teamscale-jacoco-agent.zip"
+unzip -d tmp "$profiler_dist"
+unzip -d tmp build/distributions/sample-app.zip
+JAVA_TOOL_OPTIONS="-javaagent:tmp/teamscale-jacoco-agent/lib/teamscale-jacoco-agent.jar" tmp/sample-app/bin/sample-app
