41b5dfe51cc1ea4b9090797f6477eb3e838ff1bb
Fabian Streitel <streitel@cqse.eu>
2018-09-22 18:28:49 +0200

    add incompatible logging to sample app

--- sample-app/src/main/java/Main.java
+++ sample-app/src/main/java/Main.java
@@ -1,7 +1,9 @@
+import org.apache.logging.log4j.LogManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		LogManager.getLogger("testlogger").info("testing logging with incompatible log4j version");
 	}
 
 }
