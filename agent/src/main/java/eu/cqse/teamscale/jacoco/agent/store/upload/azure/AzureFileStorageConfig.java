197001e0fec8ebe39553b630bbbd6ea4fd94eecb
Timo Pawelka <pawelka@cqse.eu>
2018-11-02 11:07:37 +0100

    Added comments

--- agent/src/main/java/eu/cqse/teamscale/jacoco/agent/store/upload/azure/AzureFileStorageConfig.java
+++ agent/src/main/java/eu/cqse/teamscale/jacoco/agent/store/upload/azure/AzureFileStorageConfig.java
@@ -2,9 +2,12 @@ package eu.cqse.teamscale.jacoco.agent.store.upload.azure;
 
 import okhttp3.HttpUrl;
 
-/** Confing necessary to upload files to an azure file storage. */
+/** Config necessary to upload files to an azure file storage. */
 public class AzureFileStorageConfig {
+	/** The URL to the azure file storage */
 	public HttpUrl url;
+
+	/** The access key of the azure file storage */
 	public String accessKey;
 
 	/** Checks if none of the required fields is null. */
