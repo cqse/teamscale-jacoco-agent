5b0b409bc09a542bbfc0c4b567617735ec854168
Florian Dreier <dreier@cqse.eu>
2019-01-09 07:56:39 +0100

    Attempt to fix tests

--- teamscale-gradle-plugin/src/main/kotlin/com/teamscale/BuildVersion.kt
+++ teamscale-gradle-plugin/src/main/kotlin/com/teamscale/BuildVersion.kt
@@ -5,7 +5,7 @@ object BuildVersion {
 
     /** Extracts the plugin's version from the jar's manifest file. */
     val buildVersion: String
-        get() = BuildVersion::class.java.getPackage().implementationVersion ?: "0.3.1"
+        get() = BuildVersion::class.java.getPackage().implementationVersion ?: "0.3.3"
 
     /** Extracts the agent's version from the jar's manifest file. */
     val agentVersion: String
