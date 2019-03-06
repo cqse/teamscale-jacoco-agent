5597a5c9fab51adab8d08f0c60cb76b716de79a4
Albert Steckermeier <albert.steckermeier@gmail.com>
2019-03-07 00:10:12 +0100

    Add more documentation

--- impacted-test-engine/src/main/java/com/teamscale/test_impacted/commons/IndentingWriter.java
+++ impacted-test-engine/src/main/java/com/teamscale/test_impacted/commons/IndentingWriter.java
@@ -1,17 +1,20 @@
 package com.teamscale.test_impacted.commons;
 
+/** Utility class for writing lines with tab indentation. */
 public class IndentingWriter {
 
 	private StringBuilder builder = new StringBuilder();
 
 	private int indent = 0;
 
+	/** Indents all {@link #writeLine(String)} calls in the indented writes by one more tab. */
 	public void indent(Runnable indentedWrites) {
 		indent++;
 		indentedWrites.run();
 		indent--;
 	}
 
+	/** Writes a new line. */
 	public void writeLine(String line) {
 		for (int i = 0; i < indent; i++) {
 			builder.append("\t");
