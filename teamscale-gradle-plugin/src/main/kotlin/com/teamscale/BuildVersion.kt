5d231290dc2116a16817025290299f35f79dd0ee
Florian Dreier <dreier@cqse.eu>
2019-07-01 14:18:47 +0200

    Increase plugin version

--- teamscale-gradle-plugin/src/main/kotlin/com/teamscale/BuildVersion.kt
+++ teamscale-gradle-plugin/src/main/kotlin/com/teamscale/BuildVersion.kt
@@ -5,7 +5,7 @@ object BuildVersion {
 
     /** Extracts the plugin's version from the jar's manifest file. */
     val buildVersion: String
-        get() = "0.4.12"
+        get() = "0.4.13"
 
     /** Extracts the agent's version from the jar's manifest file. */
     val agentVersion: String
