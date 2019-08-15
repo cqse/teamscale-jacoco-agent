9fe103882cc0370afad3da33b4a2d7bcb19acc08
Fabian Streitel <streitel@cqse.eu>
2019-08-15 17:43:39 +0200

    fix travis ci build (oraclejdk8 no longer available)

--- sample-app/run-with-profiler.sh
+++ sample-app/run-with-profiler.sh
@@ -3,4 +3,4 @@ profiler_dist="../agent/build/distributions/teamscale-jacoco-agent.zip"
 rm -r tmp
 unzip -d tmp "$profiler_dist"
 unzip -d tmp build/distributions/sample-app.zip
-JAVA_TOOL_OPTIONS="-javaagent:tmp/teamscale-jacoco-agent/lib/teamscale-jacoco-agent.jar=out=tmp,class-dir=tmp" tmp/sample-app/bin/sample-app
+JAVA_TOOL_OPTIONS="-javaagent:tmp/teamscale-jacoco-agent/lib/teamscale-jacoco-agent.jar=out=tmp" tmp/sample-app/bin/sample-app
