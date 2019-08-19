b00339ca351d3fb3529d0939feae6d50e1edd751
Fabian Streitel <streitel@cqse.eu>
2019-08-19 11:41:34 +0200

    don't duplicate version info in code and build scripts
    instead write to properties file in build and read from plugin code

--- teamscale-gradle-plugin/src/main/kotlin/com/teamscale/BuildVersion.kt
+++ teamscale-gradle-plugin/src/main/kotlin/com/teamscale/BuildVersion.kt
@@ -1,14 +1,16 @@
 package com.teamscale
 
+import java.util.*
+
 /** Helper for getting the plugin version at runtime. */
 object BuildVersion {
 
+    private val bundle = ResourceBundle.getBundle("com.teamscale.plugin")
+
     /** Extracts the plugin's version from the jar's manifest file. */
-    val buildVersion: String
-        get() = "0.4.13"
+    val buildVersion: String = bundle.getString("pluginVersion")
 
     /** Extracts the agent's version from the jar's manifest file. */
-    val agentVersion: String
-        get() = "15.1.0"
+    val agentVersion: String = bundle.getString("agentVersion")
 
 }
