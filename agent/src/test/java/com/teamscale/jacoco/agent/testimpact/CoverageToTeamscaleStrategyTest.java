214496c0b47c71a0345d633521a40af58a0ffdaf
Fabian Streitel <streitel@cqse.eu>
2020-03-18 11:35:40 +0100

    add test for valid call sequence of rest endpoints

--- agent/src/test/java/com/teamscale/jacoco/agent/testimpact/CoverageToTeamscaleStrategyTest.java
+++ agent/src/test/java/com/teamscale/jacoco/agent/testimpact/CoverageToTeamscaleStrategyTest.java
@@ -1,6 +1,10 @@
 package com.teamscale.jacoco.agent.testimpact;
 
+import com.teamscale.client.ClusteredTestDetails;
 import com.teamscale.client.CommitDescriptor;
+import com.teamscale.client.EReportFormat;
+import com.teamscale.client.PrioritizableTest;
+import com.teamscale.client.PrioritizableTestCluster;
 import com.teamscale.client.TeamscaleClient;
 import com.teamscale.client.TeamscaleServer;
 import com.teamscale.jacoco.agent.JacocoRuntimeController;
@@ -12,17 +16,33 @@ import com.teamscale.report.util.ClasspathWildcardIncludeFilter;
 import okhttp3.HttpUrl;
 import org.jacoco.core.data.ExecutionDataStore;
 import org.jacoco.core.data.SessionInfo;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import retrofit2.Response;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class CoverageToTeamscaleStrategyTest {
+
+	@Rule
+	public MockitoRule mockRule = MockitoJUnit.rule();
+
+	@Mock
+	private TeamscaleClient client;
 
 	@Test
 	public void mustRunTestStartBeforeTestEnd() throws Exception {
@@ -35,6 +55,29 @@ public class CoverageToTeamscaleStrategyTest {
 		assertThrows(UnsupportedOperationException.class, strategy::testRunEnd);
 	}
 
+	@Test
+	public void testValidCallSequence() throws Exception {
+		List<PrioritizableTestCluster> clusters = Collections
+				.singletonList(new PrioritizableTestCluster("cluster",
+						Collections.singletonList(new PrioritizableTest("mytest"))));
+		when(client.getImpactedTests(any(), any(), any(), any(), anyBoolean())).thenReturn(Response.success(clusters));
+
+		AgentOptions options = mockOptions();
+		JacocoRuntimeController controller = mockController();
+		CoverageToTeamscaleStrategy strategy = new CoverageToTeamscaleStrategy(controller, options);
+
+		strategy.testRunStart(
+				Collections.singletonList(new ClusteredTestDetails("mytest", "mytest", "content", "cluster")), false,
+				null);
+		strategy.testStart("mytest");
+		strategy.testEnd("mytest", new TestExecution("mytest", 0L, ETestExecutionResult.PASSED));
+		strategy.testRunEnd();
+
+		verify(client).uploadReport(eq(EReportFormat.TESTWISE_COVERAGE),
+				eq("{\"tests\":[{\"content\":\"content\",\"duration\":0.0,\"paths\":[],\"result\":\"PASSED\",\"sourcePath\":\"mytest\",\"uniformPath\":\"mytest\"}]}"),
+				any(), any(), any());
+	}
+
 	private JacocoRuntimeController mockController() throws JacocoRuntimeController.DumpException {
 		JacocoRuntimeController controller = mock(JacocoRuntimeController.class);
 		when(controller.dumpAndReset()).thenReturn(new Dump(new SessionInfo("mytest", 0, 0), new ExecutionDataStore()));
@@ -43,6 +86,7 @@ public class CoverageToTeamscaleStrategyTest {
 
 	private AgentOptions mockOptions() throws URISyntaxException {
 		AgentOptions options = mock(AgentOptions.class);
+
 		TeamscaleServer server = new TeamscaleServer();
 		server.commit = new CommitDescriptor("branch", "12345");
 		server.url = HttpUrl.get("http://doesnt-exist.io");
@@ -50,12 +94,12 @@ public class CoverageToTeamscaleStrategyTest {
 		server.userAccessToken = "token";
 		server.partition = "partition";
 		when(options.getTeamscaleServerOptions()).thenReturn(server);
+
 		when(options.getLocationIncludeFilter()).thenReturn(new ClasspathWildcardIncludeFilter("**", ""));
-		// must have valid class files or the report generator will throw an exception
+		// must have at least one class file or the report generator will throw an exception
 		when(options.getClassDirectoriesOrZips()).thenReturn(Collections.singletonList(
 				new File(getClass().getResource("Main.class").toURI())));
 
-		TeamscaleClient client = mock(TeamscaleClient.class);
 		when(options.createTeamscaleClient()).thenReturn(client);
 		return options;
 	}
