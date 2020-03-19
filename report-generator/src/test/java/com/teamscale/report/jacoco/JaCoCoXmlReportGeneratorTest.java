e17fc280a852917fc4407a2775bae000abe2e1e5
Thomas Pettinger <pettinger@cqse.eu>
2020-03-19 20:24:07 +0100

    CR#22116 Dynamically calculate class IDs in JaCoCoXmlReportGeneratorTest

--- report-generator/src/test/java/com/teamscale/report/jacoco/JaCoCoXmlReportGeneratorTest.java
+++ report-generator/src/test/java/com/teamscale/report/jacoco/JaCoCoXmlReportGeneratorTest.java
@@ -8,10 +8,12 @@ import com.teamscale.test.TestDataBase;
 import org.jacoco.core.data.ExecutionData;
 import org.jacoco.core.data.ExecutionDataStore;
 import org.jacoco.core.data.SessionInfo;
+import org.jacoco.core.internal.data.CRC64;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
 
@@ -53,48 +55,73 @@ public class JaCoCoXmlReportGeneratorTest extends TestDataBase {
 
 	@Test
 	void testEmptyCoverageFileThrowsException() throws IOException {
-		assertThatThrownBy(() -> runGenerator("empty-report-handling", EDuplicateClassFileBehavior.IGNORE,
-				new ClasspathWildcardIncludeFilter("some.package.*", null), false))
+		String testFolderName = "empty-report-handling";
+		long classId = calculateClassId(testFolderName, "TestClass.class");
+		assertThatThrownBy(() -> runGenerator(testFolderName, EDuplicateClassFileBehavior.IGNORE,
+				new ClasspathWildcardIncludeFilter("some.package.*", null), false, createDummyDump(classId)))
 				.isExactlyInstanceOf(EmptyReportException.class);
 	}
 
 	@Test
 	void testNonEmptyCoverageFileDoesNotThrowException() throws IOException, EmptyReportException {
-		runGenerator("empty-report-handling", EDuplicateClassFileBehavior.IGNORE,
-				new ClasspathWildcardIncludeFilter("*", null), false);
+		String testFolderName = "empty-report-handling";
+		long classId = calculateClassId(testFolderName, "TestClass.class");
+		runGenerator(testFolderName, EDuplicateClassFileBehavior.IGNORE,
+				new ClasspathWildcardIncludeFilter("*", null), false, createDummyDump(classId));
 	}
 
 	@Test
 	void testDisableEmptyCoverageException() throws IOException, EmptyReportException {
 		// Should not throw EmptyReportException if we pass true for writingEmptyReports
-		runGenerator("empty-report-handling", EDuplicateClassFileBehavior.IGNORE,
-				new ClasspathWildcardIncludeFilter("some.package.*", null), true);
+		String testFolderName = "empty-report-handling";
+		long classId = calculateClassId(testFolderName, "TestClass.class");
+		runGenerator(testFolderName, EDuplicateClassFileBehavior.IGNORE,
+				new ClasspathWildcardIncludeFilter("some.package.*", null), true, createDummyDump(classId));
 	}
 
-	/** Creates a dummy dump. */
-	private static Dump createDummyDump() {
+	/**
+	 * Creates a dummy dump with the specified class ID. The class ID can currently be calculated with {@link
+	 * org.jacoco.core.internal.data.CRC64#classId(byte[])}. This might change in the future, as it's considered an
+	 * implementation detail of JaCoCo (c.f. <a href="https://www.jacoco.org/jacoco/trunk/doc/classids.html">
+	 * https://www.jacoco.org/jacoco/trunk/doc/classids.html</a>)
+	 */
+	private static Dump createDummyDump(long classId) {
 		ExecutionDataStore store = new ExecutionDataStore();
-		store.put(new ExecutionData(6270089523198553326L, "TestClass", new boolean[]{true, true, true}));
+		store.put(new ExecutionData(classId, "TestClass", new boolean[]{true, true, true}));
 		SessionInfo info = new SessionInfo("session-id", 124L, 125L);
 		return new Dump(info, store);
+	}
+
+	/**
+	 * Creates a dummy dump with an arbitrary class ID.
+	 */
+	private static Dump createDummyDump() {
+		return createDummyDump(123);
+	}
+
+	private long calculateClassId(String testFolderName, String classFileName) throws IOException {
+		File classFile = useTestFile(testFolderName + File.separator + classFileName);
+		return CRC64.classId(Files.readAllBytes(classFile.toPath()));
 	}
 
 	/** Runs the report generator. */
 	private void runGenerator(String testDataFolder,
 							  EDuplicateClassFileBehavior duplicateClassFileBehavior) throws Exception, EmptyReportException {
-		runGenerator(testDataFolder, duplicateClassFileBehavior, new ClasspathWildcardIncludeFilter(null, null), true);
+		runGenerator(testDataFolder, duplicateClassFileBehavior, new ClasspathWildcardIncludeFilter(null, null), true,
+				createDummyDump());
 	}
 
 	private void runGenerator(String testDataFolder,
 							  EDuplicateClassFileBehavior duplicateClassFileBehavior,
 							  ClasspathWildcardIncludeFilter filter,
-							  boolean writeEmptyReportFiles) throws IOException, EmptyReportException {
+							  boolean writeEmptyReportFiles,
+							  Dump dump) throws IOException, EmptyReportException {
 		File classFileFolder = useTestFile(testDataFolder);
 		long currentTime = System.currentTimeMillis();
 		String outputFilePath = "test-coverage-" + currentTime + ".xml";
 		new JaCoCoXmlReportGenerator(Collections.singletonList(classFileFolder), filter,
 				duplicateClassFileBehavior,
-				mock(ILogger.class)).convert(createDummyDump(), Paths.get(outputFilePath), writeEmptyReportFiles);
+				mock(ILogger.class)).convert(dump, Paths.get(outputFilePath), writeEmptyReportFiles);
 	}
 
 }
