e79fdcd7eb462bef626c9d2c6ad695759f1d6988
Thomas Pettinger <pettinger@cqse.eu>
2020-03-19 19:36:56 +0100

    CR#22116 Remove unused constructors from EmptyReportException

--- report-generator/src/main/java/com/teamscale/report/jacoco/EmptyReportException.java
+++ report-generator/src/main/java/com/teamscale/report/jacoco/EmptyReportException.java
@@ -5,22 +5,7 @@ package com.teamscale.report.jacoco;
  */
 public class EmptyReportException extends Exception {
 
-	public EmptyReportException() {
-	}
-
 	public EmptyReportException(String s) {
 		super(s);
-	}
-
-	public EmptyReportException(String s, Throwable throwable) {
-		super(s, throwable);
-	}
-
-	public EmptyReportException(Throwable throwable) {
-		super(throwable);
-	}
-
-	protected EmptyReportException(String s, Throwable throwable, boolean b, boolean b1) {
-		super(s, throwable, b, b1);
 	}
 }
