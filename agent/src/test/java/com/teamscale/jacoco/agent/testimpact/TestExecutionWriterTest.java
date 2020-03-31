fbe5648fb399d16273918fa17a3e0ebd2ed5fae0
Fabian Streitel <streitel@cqse.eu>
2020-03-31 11:45:45 +0200

    use @TempDir

--- agent/src/test/java/com/teamscale/jacoco/agent/testimpact/TestExecutionWriterTest.java
+++ agent/src/test/java/com/teamscale/jacoco/agent/testimpact/TestExecutionWriterTest.java
@@ -3,6 +3,7 @@ package com.teamscale.jacoco.agent.testimpact;
 import com.teamscale.report.testwise.model.ETestExecutionResult;
 import com.teamscale.report.testwise.model.TestExecution;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,33 +13,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 class TestExecutionWriterTest {
 
 	@Test
-	public void testOneExecution() throws Exception {
-		Path tempFile = Files.createTempFile("TestExecutionWriterTest", "tmp");
-		try {
-			TestExecutionWriter writer = new TestExecutionWriter(tempFile.toFile());
-			writer.append(new TestExecution("test1", 123, ETestExecutionResult.PASSED));
-			String json = String.join("\n", Files.readAllLines(tempFile));
-			assertThat(json).isEqualTo("[{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test1\"}]");
-		} finally {
-			tempFile.toFile().delete();
-		}
+	public void testOneExecution(@TempDir Path tempDir) throws Exception {
+		Path tempFile = tempDir.resolve("executions.json");
+		TestExecutionWriter writer = new TestExecutionWriter(tempFile.toFile());
+		writer.append(new TestExecution("test1", 123, ETestExecutionResult.PASSED));
+		String json = String.join("\n", Files.readAllLines(tempFile));
+		assertThat(json).isEqualTo("[{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test1\"}]");
 	}
 
 	@Test
-	public void testMultipleExecutions() throws Exception {
-		Path tempFile = Files.createTempFile("TestExecutionWriterTest", "tmp");
-		try {
-			TestExecutionWriter writer = new TestExecutionWriter(tempFile.toFile());
-			writer.append(new TestExecution("test1", 123, ETestExecutionResult.PASSED));
-			writer.append(new TestExecution("test2", 123, ETestExecutionResult.PASSED));
-			writer.append(new TestExecution("test3", 123, ETestExecutionResult.PASSED));
-			String json = String.join("\n", Files.readAllLines(tempFile));
-			assertThat(json).isEqualTo("[{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test1\"}" +
-					",{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test2\"}" +
-					",{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test3\"}]");
-		} finally {
-			tempFile.toFile().delete();
-		}
+	public void testMultipleExecutions(@TempDir Path tempDir) throws Exception {
+		Path tempFile = tempDir.resolve("executions.json");
+		TestExecutionWriter writer = new TestExecutionWriter(tempFile.toFile());
+		writer.append(new TestExecution("test1", 123, ETestExecutionResult.PASSED));
+		writer.append(new TestExecution("test2", 123, ETestExecutionResult.PASSED));
+		writer.append(new TestExecution("test3", 123, ETestExecutionResult.PASSED));
+		String json = String.join("\n", Files.readAllLines(tempFile));
+		assertThat(json).isEqualTo("[{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test1\"}" +
+				",{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test2\"}" +
+				",{\"durationMillis\":123,\"result\":\"PASSED\",\"uniformPath\":\"test3\"}]");
 	}
 
 }
