7026f1baa4b57ea5e48ec4e6db7fd012c4f3d5d1
Thomas Pettinger <pettinger.thomas@gmail.com>
2020-08-18 11:22:37 +0200

    CR#24034 Add TestAgentOptionsBuilder and tests for getting and setting the partition

--- agent/src/test/java/com/teamscale/jacoco/agent/AgentHttpServerTest.java
+++ agent/src/test/java/com/teamscale/jacoco/agent/AgentHttpServerTest.java
@@ -3,7 +3,7 @@ package com.teamscale.jacoco.agent;
 import com.teamscale.client.TeamscaleServer;
 import com.teamscale.jacoco.agent.options.AgentOptionParseException;
 import com.teamscale.jacoco.agent.options.AgentOptions;
-import com.teamscale.jacoco.agent.options.AgentOptionsParser;
+import com.teamscale.jacoco.agent.options.TestAgentOptionsBuilder;
 import com.teamscale.jacoco.agent.upload.UploaderException;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -20,21 +20,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AgentHttpServerTest {
 
 	private Agent agent;
+	private final Integer httpServerPort = 8081;
 	private final String defaultCommitMessage = "Some Message";
+	private final String defaultPartition = "Some Partition";
 
 	/** Starts the http server to control the agent */
 	@BeforeEach
 	public void setup() throws AgentOptionParseException, UploaderException {
-		AgentOptions options = AgentOptionsParser.parse("" +
-						"teamscale-server-url=127.0.0.1," +
-						"teamscale-project=test," +
-						"teamscale-user=build," +
-						"teamscale-access-token=token," +
-						"teamscale-partition=\"Unit Tests\"," +
-						"teamscale-commit=default:HEAD," +
-						"teamscale-message=\"" + defaultCommitMessage + "\"," +
-						"http-server-port=8081",
-				null);
+		AgentOptions options = new TestAgentOptionsBuilder()
+				.withHttpServerPort(httpServerPort)
+				.withTeamscaleMessage(defaultCommitMessage)
+				.withTeamscalePartition(defaultPartition)
+				.create();
+
 		// Starts the HTTP server for the agent
 		agent = new Agent(options, null);
 	}
@@ -50,7 +48,7 @@ public class AgentHttpServerTest {
 	public void testOverridingMessage() throws IOException {
 		String newMessage = "New Message";
 
-		overrideMessage(newMessage);
+		overrideEndpoint("message", newMessage);
 
 		TeamscaleServer teamscaleServer = agent.options.getTeamscaleServerOptions();
 		assertThat(teamscaleServer.message).isEqualTo(newMessage);
@@ -59,24 +57,44 @@ public class AgentHttpServerTest {
 	/** Test reading the commit message */
 	@Test
 	public void testGettingMessage() throws IOException {
-		String receivedMessage = getMessage();
+		String receivedMessage = readFromEndpoint("message");
 
 		assertThat(receivedMessage).isEqualTo(defaultCommitMessage);
 	}
 
-	private void overrideMessage(String newMessage) throws IOException {
+	/** Test overwriting the partition */
+	@Test
+	public void testOverridingPartition() throws IOException {
+		String newPartition = "New Partition";
+
+		overrideEndpoint("partition", newPartition);
+
+		TeamscaleServer teamscaleServer = agent.options.getTeamscaleServerOptions();
+		assertThat(teamscaleServer.partition).isEqualTo(newPartition);
+	}
+
+	/** Test reading the partition */
+	@Test
+	public void testGettingPartition() throws IOException {
+		String receivedPartition = readFromEndpoint("partition");
+
+		assertThat(receivedPartition).isEqualTo(defaultPartition);
+	}
+
+
+	private void overrideEndpoint(String endpointPath, String newMessage) throws IOException {
 		OkHttpClient client = new OkHttpClient();
 		Request request = new Request.Builder()
-				.url("http://localhost:8081/message/" + newMessage)
+				.url("http://localhost:" + httpServerPort + "/" + endpointPath + "/" + newMessage)
 				.method("POST", RequestBody.create(null, new byte[0]))
 				.build();
 		client.newCall(request).execute();
 	}
 
-	private String getMessage() throws IOException {
+	private String readFromEndpoint(String endpointPath) throws IOException {
 		OkHttpClient client = new OkHttpClient();
 		Request request = new Request.Builder()
-				.url("http://localhost:8081/message")
+				.url("http://localhost:" + httpServerPort + "/" + endpointPath)
 				.build();
 		Response response = client.newCall(request).execute();
 		return response.body() != null ? response.body().string() : "";
