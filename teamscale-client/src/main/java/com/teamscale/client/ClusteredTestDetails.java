5d7768d7f4cd8dee245e2b25ab9bd6d32210b9f7
Fabian Streitel <streitel@cqse.eu>
2020-09-08 14:12:55 +0200

    provide API to easily hash test data when constructing ClusteredTestDetails

--- teamscale-client/src/main/java/com/teamscale/client/ClusteredTestDetails.java
+++ teamscale-client/src/main/java/com/teamscale/client/ClusteredTestDetails.java
@@ -1,14 +1,14 @@
 package com.teamscale.client;
 
 /**
- * {@link TestDetails} with additional information about which cluster of tests
- * the test case belongs to during prioritization.
+ * {@link TestDetails} with additional information about which cluster of tests the test case belongs to during
+ * prioritization.
  */
 public class ClusteredTestDetails extends TestDetails {
 
 	/**
-	 * A unique identifier for the cluster this test should be prioritized within.
-	 * If null the test get's assigned it's own cluster.
+	 * A unique identifier for the cluster this test should be prioritized within. If null the test gets assigned it's
+	 * own unique cluster.
 	 */
 	public String clusterId;
 
@@ -16,5 +16,20 @@ public class ClusteredTestDetails extends TestDetails {
 		super(uniformPath, sourcePath, content);
 		this.clusterId = clusterId;
 	}
+
+	/**
+	 * Creates clustered test details with the given additional {@link TestData}.
+	 * <p>
+	 * Use this to easily mark additional files or data as belonging to that test case. Whenever the given {@link
+	 * TestData} changes, this test will be selected to be run by the TIA.
+	 * <p>
+	 * Example: For a test that reads test data from an XML file, you should pass the contents of that XML file as its
+	 * test data. Then, whenever the XML is modified, the corresponding test will be run by the TIA.
+	 */
+	public ClusteredTestDetails(String uniformPath, String sourcePath, TestData testData, String clusterId) {
+		super(uniformPath, sourcePath, testData.hash);
+		this.clusterId = clusterId;
+	}
+
 }
 
