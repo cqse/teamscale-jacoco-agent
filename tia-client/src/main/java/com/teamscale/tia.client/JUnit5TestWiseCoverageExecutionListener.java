44b4f1d70c360ba1004e2f08228af8766a529aa2
Fabian Streitel <streitel@cqse.eu>
2022-03-01 14:07:27 +0100

    reuse logging code for both run listeners
    junit 4 and 5

--- tia-client/src/main/java/com/teamscale/tia.client/JUnit5TestWiseCoverageExecutionListener.java
+++ tia-client/src/main/java/com/teamscale/tia.client/JUnit5TestWiseCoverageExecutionListener.java
@@ -1,7 +1,6 @@
 package com.teamscale.tia.client;
 
 import com.teamscale.report.testwise.model.ETestExecutionResult;
-import okhttp3.HttpUrl;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.TestSource;
 import org.junit.platform.engine.support.descriptor.ClassSource;
@@ -9,9 +8,6 @@ import org.junit.platform.engine.support.descriptor.MethodSource;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
-import org.junit.runner.notification.RunListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
@@ -21,50 +17,8 @@ import java.util.Optional;
 @SuppressWarnings("unused")
 public class JUnit5TestWiseCoverageExecutionListener implements TestExecutionListener {
 
-	private final TestRun testRun;
-	private RunningTest runningTest;
-	private final Logger logger = LoggerFactory.getLogger(JUnit5TestWiseCoverageExecutionListener.class);
-
-	private static class RunListenerConfigurationException extends RuntimeException {
-		public RunListenerConfigurationException(String message) {
-			super(message);
-		}
-	}
-
-	public JUnit5TestWiseCoverageExecutionListener() {
-		logger.debug(getClass().getName() + " instantiated");
-		String agentUrl = System.getProperty("tia.agent");
-		if (agentUrl == null) {
-			agentUrl = System.getenv("TIA_AGENT");
-		}
-		if (agentUrl == null) {
-			throw new RunListenerConfigurationException(
-					"You did not provide the URL of a Teamscale JaCoCo agent that will record test-wise coverage." +
-							" You can configure the URL either as a system property with -Dtia.agent=URL" +
-							" or as an environment variable with TIA_AGENT=URL.");
-		}
-
-		TiaAgent agent = new TiaAgent(false, HttpUrl.get(agentUrl));
-		testRun = agent.startTestRunWithoutTestSelection();
-	}
-
-	@FunctionalInterface
-	private interface Action {
-		/** Runs the action, throwing exceptions if it fails. */
-		void run() throws Exception;
-	}
-
-	/**
-	 * We mustn't throw exceptions out of the {@link RunListener} interface methods or Maven will treat the test as
-	 * failed. And we don't have access to the Maven logger, so we just log to stderr.
-	 */
-	private void handleErrors(Action action, String description) {
-		try {
-			action.run();
-		} catch (Exception e) {
-			logger.error("Encountered an error while recording test-wise coverage in step: " + description, e);
-		}
-	}
+	private final RunListenerAgentBridge bridge = new RunListenerAgentBridge(
+			JUnit5TestWiseCoverageExecutionListener.class.getName());
 
 	@Override
 	public void executionStarted(TestIdentifier testIdentifier) {
@@ -72,8 +26,7 @@ public class JUnit5TestWiseCoverageExecutionListener implements TestExecutionLis
 			return;
 		}
 		String uniformPath = getUniformPath(testIdentifier);
-		logger.debug("Started test '{}'", uniformPath);
-		handleErrors(() -> runningTest = testRun.startTest(uniformPath), "Starting test '" + uniformPath + "'");
+		bridge.testStarted(uniformPath);
 	}
 
 	private String getUniformPath(TestIdentifier testIdentifier) {
@@ -98,26 +51,21 @@ public class JUnit5TestWiseCoverageExecutionListener implements TestExecutionLis
 			return;
 		}
 		String uniformPath = getUniformPath(testIdentifier);
-		logger.debug("Finished test '{}'", uniformPath);
-		handleErrors(() -> {
-			if (runningTest != null) {
-				ETestExecutionResult result;
-				switch (testExecutionResult.getStatus()) {
-					case SUCCESSFUL:
-						result = ETestExecutionResult.PASSED;
-						break;
-					case ABORTED:
-						result = ETestExecutionResult.ERROR;
-						break;
-					case FAILED:
-					default:
-						result = ETestExecutionResult.FAILURE;
-						break;
-				}
-				runningTest.endTest(new TestRun.TestResultWithMessage(result, null));
-				runningTest = null;
-			}
-		}, "Finishing test '" + uniformPath + "'");
+		ETestExecutionResult result;
+		switch (testExecutionResult.getStatus()) {
+			case SUCCESSFUL:
+				result = ETestExecutionResult.PASSED;
+				break;
+			case ABORTED:
+				result = ETestExecutionResult.ERROR;
+				break;
+			case FAILED:
+			default:
+				result = ETestExecutionResult.FAILURE;
+				break;
+		}
+
+		bridge.testFinished(uniformPath, result);
 	}
 
 	@Override
@@ -126,18 +74,11 @@ public class JUnit5TestWiseCoverageExecutionListener implements TestExecutionLis
 			return;
 		}
 		String uniformPath = getUniformPath(testIdentifier);
-		logger.debug("Skipped test '{}'", uniformPath);
-		handleErrors(() -> {
-			if (runningTest != null) {
-				runningTest.endTest(new TestRun.TestResultWithMessage(ETestExecutionResult.SKIPPED, null));
-				runningTest = null;
-			}
-		}, "Skipping test '" + uniformPath + "'");
+		bridge.testSkipped(uniformPath, reason);
 	}
 
 	@Override
 	public void testPlanExecutionFinished(TestPlan testPlan) {
-		logger.debug("Finished test run");
-		handleErrors(testRun::endTestRun, "Finishing the test run");
+		bridge.testRunFinished();
 	}
 }
--- tia-client/src/main/java/com/teamscale/tia.client/TestWiseCoverageRunListener.java
+++ tia-client/src/main/java/com/teamscale/tia.client/TestWiseCoverageRunListener.java
@@ -1,7 +1,6 @@
 package com.teamscale.tia.client;
 
 import com.teamscale.report.testwise.model.ETestExecutionResult;
-import okhttp3.HttpUrl;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -13,53 +12,13 @@ import org.junit.runner.notification.RunListener;
 @SuppressWarnings("unused")
 public class TestWiseCoverageRunListener extends RunListener {
 
-	private final TestRun testRun;
-	private RunningTest runningTest;
-
-	private static class RunListenerConfigurationException extends RuntimeException {
-		public RunListenerConfigurationException(String message) {
-			super(message);
-		}
-	}
-
-	public TestWiseCoverageRunListener() {
-		String agentUrl = System.getProperty("tia.agent");
-		if (agentUrl == null) {
-			agentUrl = System.getenv("TIA_AGENT");
-		}
-		if (agentUrl == null) {
-			throw new RunListenerConfigurationException(
-					"You did not provide the URL of a Teamscale JaCoCo agent that will record test-wise coverage." +
-							" You can configure the URL either as a system property with -Dtia.agent=URL" +
-							" or as an environment variable with TIA_AGENT=URL.");
-		}
-
-		TiaAgent agent = new TiaAgent(false, HttpUrl.get(agentUrl));
-		testRun = agent.startTestRunWithoutTestSelection();
-	}
-
-	@FunctionalInterface
-	private interface Action {
-		/** Runs the action, throwing exceptions if it fails. */
-		void run() throws Exception;
-	}
-
-	/**
-	 * We mustn't throw exceptions out of the {@link RunListener} interface methods or Maven will treat the test as
-	 * failed. And we don't have access to the Maven logger, so we just log to stderr.
-	 */
-	private void handleErrors(Action action) {
-		try {
-			action.run();
-		} catch (Exception e) {
-			System.err.println("Encountered an error while recording test-wise coverage:");
-			e.printStackTrace(System.err);
-		}
-	}
+	private final RunListenerAgentBridge bridge = new RunListenerAgentBridge(
+			TestWiseCoverageRunListener.class.getName());
 
 	@Override
 	public void testStarted(Description description) {
-		handleErrors(() -> runningTest = testRun.startTest(getUniformPath(description)));
+		String uniformPath = getUniformPath(description);
+		bridge.testStarted(uniformPath);
 	}
 
 	private String getUniformPath(Description description) {
@@ -72,38 +31,30 @@ public class TestWiseCoverageRunListener extends RunListener {
 
 	@Override
 	public void testFinished(Description description) {
-		handleErrors(() -> {
-			if (runningTest != null) {
-				runningTest.endTest(new TestRun.TestResultWithMessage(ETestExecutionResult.PASSED, null));
-				runningTest = null;
-			}
-		});
+		String uniformPath = getUniformPath(description);
+		bridge.testFinished(uniformPath, ETestExecutionResult.PASSED);
 	}
 
 	@Override
 	public void testFailure(Failure failure) {
-		handleErrors(() -> {
-			if (runningTest != null) {
-				runningTest.endTest(
-						new TestRun.TestResultWithMessage(ETestExecutionResult.FAILURE, failure.getTrace()));
-				runningTest = null;
-			}
-		});
+		String uniformPath = getUniformPath(failure.getDescription());
+		bridge.testFinished(uniformPath, ETestExecutionResult.FAILURE, failure.getMessage());
 	}
 
 	@Override
 	public void testAssumptionFailure(Failure failure) {
-		handleErrors(() -> {
-			if (runningTest != null) {
-				runningTest.endTest(
-						new TestRun.TestResultWithMessage(ETestExecutionResult.FAILURE, failure.getTrace()));
-				runningTest = null;
-			}
-		});
+		String uniformPath = getUniformPath(failure.getDescription());
+		bridge.testFinished(uniformPath, ETestExecutionResult.FAILURE);
+	}
+
+	@Override
+	public void testIgnored(Description description) {
+		String uniformPath = getUniformPath(description);
+		bridge.testSkipped(uniformPath, null);
 	}
 
 	@Override
 	public void testRunFinished(Result result) {
-		handleErrors(testRun::endTestRun);
+		bridge.testRunFinished();
 	}
 }
