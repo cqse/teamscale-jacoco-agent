8f06ca5fd4ade6e5e4337954816bedbdfd3b884e
Andreas Punz <punz@cqse.eu>
2022-08-02 15:06:43 +0200

    TS-30956: rework

--- teamscale-client/src/main/java/com/teamscale/client/ClusteredTestDetails.java
+++ teamscale-client/src/main/java/com/teamscale/client/ClusteredTestDetails.java
@@ -25,5 +25,19 @@ public class ClusteredTestDetails extends TestDetails {
 		this.partition = partition;
 	}
 
+	/**
+	 * Creates clustered test details with the given additional {@link TestData}.
+	 * <p>
+	 * Use this to easily mark additional files or data as belonging to that test case. Whenever the given
+	 * {@link TestData} changes, this test will be selected to be run by the TIA.
+	 * <p>
+	 * Example: For a test that reads test data from an XML file, you should pass the contents of that XML file as its
+	 * test data. Then, whenever the XML is modified, the corresponding test will be run by the TIA.
+	 */
+	public static ClusteredTestDetails createWithTestData(String uniformPath, String sourcePath, TestData testData,
+														  String clusterId, String partition) {
+		return new ClusteredTestDetails(uniformPath, sourcePath, testData.hash, clusterId, partition);
+	}
+
 }
 
