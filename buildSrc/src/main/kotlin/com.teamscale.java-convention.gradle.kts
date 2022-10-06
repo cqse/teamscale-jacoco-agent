58b8bb4403bca986a5c867e7d12b0150436434cd
Florian Dreier <dreier@cqse.eu>
2022-10-06 08:12:53 +0200

    TS-31709 Dependecy updates

--- buildSrc/src/main/kotlin/com.teamscale.java-convention.gradle.kts
+++ buildSrc/src/main/kotlin/com.teamscale.java-convention.gradle.kts
@@ -23,12 +23,12 @@ tasks.named<Test>("test") {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
-    testImplementation("org.mockito:mockito-core:4.5.1")
-    testImplementation("org.mockito:mockito-junit-jupiter:4.5.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("org.mockito:mockito-core:4.8.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
 
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.1")
 }
