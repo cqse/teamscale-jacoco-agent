daa2e4861a121ec8a4336fb772a2e56f23c2a6df
Alessandro Escher <escher@cqse.eu>
2023-04-12 15:41:22 +0200

    TS-8032 Added interface comment

--- teamscale-maven-plugin/src/main/java/com/teamscale/maven/upload/CoverageUploadMojo.java
+++ teamscale-maven-plugin/src/main/java/com/teamscale/maven/upload/CoverageUploadMojo.java
@@ -10,6 +10,11 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 /**
  * Binds to the VERIFY phase in which the Jacoco plugin generates its report.
  * Needs to be specified after the Jacoco goal to ensure that it is run once the Jacoco report goal has completed.
+ * Offers the following functionality:
+ * <ol>
+ *     <li>Validate Jacoco Maven plugin configuration</li>
+ *     <li>Locate and upload all reports in one session</li>
+ * </ol>
  * @see <a href="https://www.jacoco.org/jacoco/trunk/doc/report-mojo.html">Jacoco Report goal</a>
  */
 @Mojo(name = "upload-coverage", defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.RUNTIME,
