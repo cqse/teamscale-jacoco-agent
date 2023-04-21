fbf236df65633b9c70b26e4ce5e0c56a901a9504
Alessandro Escher <alessandroescher@gmx.net>
2023-04-21 13:59:23 +0200

    TS-8032 Read output directory and added TeamscaleClient

--- teamscale-maven-plugin/src/main/java/com/teamscale/maven/upload/CoverageUploadMojo.java
+++ teamscale-maven-plugin/src/main/java/com/teamscale/maven/upload/CoverageUploadMojo.java
@@ -4,8 +4,14 @@ import com.teamscale.maven.TeamscaleMojoBase;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
+import shadow.com.teamscale.client.TeamscaleClient;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 /**
  * Binds to the VERIFY phase in which the Jacoco plugin generates its report.
@@ -23,22 +29,46 @@ public class CoverageUploadMojo extends TeamscaleMojoBase {
 
 	private final static String JACOCO_PLUGIN_NAME = "org.jacoco:jacoco-maven-plugin";
 
+	@Parameter(defaultValue = "${project.reporting.outputDirectory}/jacoco/jacoco.xml")
+	private Path reportGoalOutputDirectory;
+
+	@Parameter(defaultValue = "${project.reporting.outputDirectory}/jacoco-it/jacoco.xml")
+	private Path reportIntegrationGoalOutputDirectory;
+
+	@Parameter(defaultValue = "${project.reporting.outputDirectory}/jacoco-aggregate/jacoco.xml")
+	private Path reportAggregateGoalOutputDirectory;
+
+	/**
+	 * The Teamscale client that is used to upload reports to a Teamscale instance.
+	 */
+	private TeamscaleClient teamscaleClient;
+
 	@Override
 	public void execute() throws MojoFailureException {
 		if (skip) {
 			return;
 		}
-		validateJacocoConfiguration();
+		teamscaleClient = new TeamscaleClient(teamscaleUrl, username, accessToken, projectId);
+		parseJacocoConfiguration();
 	}
 
-	private void validateJacocoConfiguration() throws MojoFailureException {
+	/**
+	 * Check that Jacoco is set up correctly and read any custom settings that may have been set.
+	 * @throws MojoFailureException If Jacoco is not set up correctly
+	 */
+	private void parseJacocoConfiguration() throws MojoFailureException {
 		// If a Dom is null it means the execution goal uses default parameters which work correctly
-		Xpp3Dom reportConfigurationDom = getExecutionConfigurationDom(JACOCO_PLUGIN_NAME, "report");
+		Xpp3Dom reportConfigurationDom = getJacocoGoalExecutionConfiguration("report");
 		validateReportFormat(reportConfigurationDom, "report");
-		Xpp3Dom reportIntegrationConfigurationDom = getExecutionConfigurationDom(JACOCO_PLUGIN_NAME, "report-integration");
+		reportGoalOutputDirectory = getCustomOutputDirectory(reportConfigurationDom).orElse(reportGoalOutputDirectory);
+
+		Xpp3Dom reportIntegrationConfigurationDom = getJacocoGoalExecutionConfiguration("report-integration");
 		validateReportFormat(reportIntegrationConfigurationDom, "report-integration");
-		Xpp3Dom reportAggregateConfigurationDom = getExecutionConfigurationDom(JACOCO_PLUGIN_NAME, "report-aggregate");
+		reportIntegrationGoalOutputDirectory = getCustomOutputDirectory(reportIntegrationConfigurationDom).orElse(reportIntegrationGoalOutputDirectory);
+
+		Xpp3Dom reportAggregateConfigurationDom = getJacocoGoalExecutionConfiguration("report-aggregate");
 		validateReportFormat(reportAggregateConfigurationDom, "report-aggregate");
+		reportAggregateGoalOutputDirectory = getCustomOutputDirectory(reportAggregateConfigurationDom).orElse(reportAggregateGoalOutputDirectory);
 	}
 
 	/**
@@ -48,17 +78,29 @@ public class CoverageUploadMojo extends TeamscaleMojoBase {
 	 * @throws MojoFailureException If the goal is not set up to generate XML reports
 	 */
 	private void validateReportFormat(Xpp3Dom configurationDom, String pluginGoal) throws MojoFailureException {
-		if (configurationDom != null && configurationDom.getChild("formats") != null) {
-			boolean producesXMLReport = false;
-			for (Xpp3Dom format : configurationDom.getChild("formats").getChildren()) {
-				if (format.getValue().equals("XML")) {
-					producesXMLReport = true;
-					break;
-				}
-			}
-			if (!producesXMLReport) {
-				throw new MojoFailureException(JACOCO_PLUGIN_NAME + " is not configured to produce XML reports for goal " + pluginGoal);
+		if (configurationDom == null || configurationDom.getChild("formats") == null) {
+			return;
+		}
+		boolean producesXMLReport = false;
+		for (Xpp3Dom format : configurationDom.getChild("formats").getChildren()) {
+			if (format.getValue().equals("XML")) {
+				producesXMLReport = true;
+				break;
 			}
 		}
+		if (!producesXMLReport) {
+			throw new MojoFailureException(JACOCO_PLUGIN_NAME + " is not configured to produce XML reports for goal " + pluginGoal);
+		}
+	}
+
+	private Optional<Path> getCustomOutputDirectory(Xpp3Dom configurationDom) {
+		if (configurationDom != null && configurationDom.getChild("outputDirectory") != null) {
+			return Optional.of(Paths.get(configurationDom.getChild("outputDirectory").getValue()));
+		}
+		return Optional.empty();
+	}
+
+	private Xpp3Dom getJacocoGoalExecutionConfiguration(String pluginGoal) {
+		return super.getExecutionConfigurationDom(JACOCO_PLUGIN_NAME, pluginGoal);
 	}
 }
