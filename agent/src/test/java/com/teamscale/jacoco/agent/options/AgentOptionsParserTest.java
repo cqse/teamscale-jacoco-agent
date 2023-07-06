7d0d428c02a631d00d106e5ef4ed0cb74cff129a
Fabian Streitel <streitel@cqse.eu>
2023-07-06 14:05:05 +0200

    test option parsing with new env variable

--- agent/src/test/java/com/teamscale/jacoco/agent/options/AgentOptionsParserTest.java
+++ agent/src/test/java/com/teamscale/jacoco/agent/options/AgentOptionsParserTest.java
@@ -3,11 +3,17 @@ package com.teamscale.jacoco.agent.options;
 import com.teamscale.jacoco.agent.util.TestUtils;
 import com.teamscale.report.util.CommandLineLogger;
 import okhttp3.HttpUrl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /** Tests parsing of the agent's command line options. */
 public class AgentOptionsParserTest {
@@ -15,13 +21,20 @@ public class AgentOptionsParserTest {
 	private final AgentOptionsParser parser = new AgentOptionsParser(new CommandLineLogger(), null);
 
 	@Test
-	public void environmentConfigOverridesCommandLineOptions() {
-		// TODO (FS)
+	public void environmentConfigOverridesCommandLineOptions(@TempDir Path tempDir) throws IOException, AgentOptionParseException {
+		Path config = tempDir.resolve("config.properties");
+		Files.write(config, "debug=true".getBytes(StandardCharsets.UTF_8));
+		AgentOptionsParser parser = new AgentOptionsParser(new CommandLineLogger(), config.toString());
+		AgentOptions options = parser.parse("debug=false");
+
+		assertThat(options.debugLogging).isEqualTo(true);
 	}
 
 	@Test
 	public void environmentConfigPathDoesNotExist() {
-		// TODO (FS)
+		assertThatThrownBy(() -> {
+			new AgentOptionsParser(new CommandLineLogger(), "/this/file/doesnt/exist").parse("");
+		}).hasMessageContaining("not found");
 	}
 
 	@Test
@@ -32,35 +45,35 @@ public class AgentOptionsParserTest {
 
 	@Test
 	public void mustPreserveDefaultExcludes() throws Exception {
-		Assertions.assertThat(parser.parse("").jacocoExcludes).isEqualTo(AgentOptions.DEFAULT_EXCLUDES);
-		Assertions.assertThat(parser.parse("excludes=**foo**").jacocoExcludes)
+		assertThat(parser.parse("").jacocoExcludes).isEqualTo(AgentOptions.DEFAULT_EXCLUDES);
+		assertThat(parser.parse("excludes=**foo**").jacocoExcludes)
 				.isEqualTo("**foo**:" + AgentOptions.DEFAULT_EXCLUDES);
 	}
 
 	@Test
 	public void mustDoHttpsRewriteForNoSchemePort443() throws Exception {
-		Assertions.assertThat(parser.parse(
-						"upload-url=teamscale.url.com:443").uploadUrl)
+		assertThat(parser.parse(
+				"upload-url=teamscale.url.com:443").uploadUrl)
 				.isEqualTo(HttpUrl.parse("https://teamscale.url.com:443/"));
 	}
 
 	@Test
 	public void mustNotDoHttpsRewriteForSchemePort443() throws Exception {
-		Assertions.assertThat(parser.parse(
-						"upload-url=http://teamscale.url.com:443").uploadUrl)
+		assertThat(parser.parse(
+				"upload-url=http://teamscale.url.com:443").uploadUrl)
 				.isEqualTo(HttpUrl.parse("http://teamscale.url.com:443/"));
 	}
 
 	@Test
 	public void defaultHttpRewriteForNoScheme() throws Exception {
-		Assertions.assertThat(parser.parse(
-						"upload-url=teamscale.url.com:8080").uploadUrl)
+		assertThat(parser.parse(
+				"upload-url=teamscale.url.com:8080").uploadUrl)
 				.isEqualTo(HttpUrl.parse("http://teamscale.url.com:8080/"));
-		Assertions.assertThat(parser.parse(
-						"upload-url=teamscale.url.com:80").uploadUrl)
+		assertThat(parser.parse(
+				"upload-url=teamscale.url.com:80").uploadUrl)
 				.isEqualTo(HttpUrl.parse("http://teamscale.url.com:80/"));
-		Assertions.assertThat(parser.parse(
-						"upload-url=teamscale.url.com:444").uploadUrl)
+		assertThat(parser.parse(
+				"upload-url=teamscale.url.com:444").uploadUrl)
 				.isEqualTo(HttpUrl.parse("http://teamscale.url.com:444/"));
 	}
 
