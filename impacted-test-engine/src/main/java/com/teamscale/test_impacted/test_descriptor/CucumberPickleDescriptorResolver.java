aa73307d66318558923d0b0e83c7836f10c2bc1c
Thomas Pettinger <pettinger@cqse.eu>
2023-08-28 17:16:35 +0200

    TS-34497 Implement delegation from JUnitPlatformSuiteDescriptorResolver to CucumberPickleDescriptorResolver

--- impacted-test-engine/src/main/java/com/teamscale/test_impacted/test_descriptor/CucumberPickleDescriptorResolver.java
+++ impacted-test-engine/src/main/java/com/teamscale/test_impacted/test_descriptor/CucumberPickleDescriptorResolver.java
@@ -2,24 +2,48 @@ package com.teamscale.test_impacted.test_descriptor;
 
 import com.teamscale.test_impacted.commons.LoggerUtils;
 import org.junit.platform.engine.TestDescriptor;
-import sun.management.Agent;
+import org.junit.platform.engine.UniqueId;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class CucumberPickleDescriptorResolver implements ITestDescriptorResolver {
+	private static final Logger LOGGER = LoggerUtils.getLogger(JUnitPlatformSuiteDescriptorResolver.class);
+
 	@Override
 	public Optional<String> getUniformPath(TestDescriptor testDescriptor) {
-		return Optional.empty();
+		LOGGER.info("Resolving cucumber uniform path");
+		List<UniqueId.Segment> cucumberSegments = getCucumberSegments(testDescriptor);
+		List<String> uniformPathSegments = cucumberSegments.stream()
+				.map(segment -> segment.getType() + "-" + segment.getValue())
+				.map(uniformPathSegment -> uniformPathSegment.replaceAll("classpath:", "").replaceAll("/", "."))
+				.collect(Collectors.toList());
+		return Optional.of(String.join("/", uniformPathSegments));
 	}
 
 	@Override
 	public Optional<String> getClusterId(TestDescriptor testDescriptor) {
-		return Optional.empty();
+		LOGGER.info("Resolving cucumber cluster id");
+		List<UniqueId.Segment> cucumberSegments = getCucumberSegments(testDescriptor);
+		List<String> uniformPathSegments = cucumberSegments.stream().map(UniqueId.Segment::getValue)
+				.collect(Collectors.toList());
+		return Optional.of(String.join("/", uniformPathSegments));
 	}
 
 	@Override
 	public String getEngineId() {
 		return "cucumber";
+	}
+
+	private List<UniqueId.Segment> getCucumberSegments(TestDescriptor testDescriptor) {
+		List<UniqueId.Segment> allSegments = testDescriptor.getUniqueId().getSegments();
+		// TODO replace strings with constants
+		UniqueId.Segment cucumberEngineSegment = allSegments.stream()
+				.filter(segment -> segment.getType().equals("engine") && segment.getValue().equals("cucumber"))
+				.findFirst().get();
+		int indexOfCucumberEngineSegment = allSegments.indexOf(cucumberEngineSegment);
+		return allSegments.subList(indexOfCucumberEngineSegment + 1, allSegments.size());
 	}
 }
--- impacted-test-engine/src/main/java/com/teamscale/test_impacted/test_descriptor/JUnitPlatformSuiteDescriptorResolver.java
+++ impacted-test-engine/src/main/java/com/teamscale/test_impacted/test_descriptor/JUnitPlatformSuiteDescriptorResolver.java
@@ -1,22 +1,102 @@
 package com.teamscale.test_impacted.test_descriptor;
 
+import com.teamscale.test_impacted.commons.LoggerUtils;
 import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.UniqueId;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class JUnitPlatformSuiteDescriptorResolver implements ITestDescriptorResolver {
+
+	private static final Logger LOGGER = LoggerUtils.getLogger(JUnitPlatformSuiteDescriptorResolver.class);
+
 	@Override
 	public Optional<String> getUniformPath(TestDescriptor testDescriptor) {
 		// Delegate to nested engines via the TestDescriptorResolverRegistry like this:
-		// return Optional.of("suite-name/" + TestDescriptorResolverRegistry.getTestDescriptorResolver(engineID).getUniformPath());
-		return Optional.empty();
+		// return Optional.of(TestDescriptorResolverRegistry.getTestDescriptorResolver(engineID).getUniformPath());
+		// TODO can we remove the clone here?
+		LOGGER.info(testDescriptor.toString());
+		List<UniqueId.Segment> segments = testDescriptor.getUniqueId().getSegments();
+		// TODO refactor stings to constants (or reuse the ones we already have)
+		if (segments.size() < 3 || !segments.get(0).getType().equals("engine") || !segments.get(1).getType()
+				.equals("suite") || !segments.get(2).getType().equals("engine")) {
+			// TODO Do we actually want to use this default or do we want to crash?
+			LOGGER.severe(() ->
+					"Assuming structure [engine:junit-platform-suite]/[suite:mySuite]/[engine:anotherEngine] " +
+							"for junit-platform-suite tests. Using "
+							+ testDescriptor.getUniqueId()
+							.toString() + "as uniform path as fallback.");
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		UniqueId.Segment suiteSegment = segments.get(1);
+		List<UniqueId.Segment> secondaryEngineSegments = segments.subList(2, segments.size());
+
+		if (secondaryEngineSegments.isEmpty() || !secondaryEngineSegments.get(0).getType().equals("engine")) {
+			// TODO log error
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		ITestDescriptorResolver secondaryTestDescriptorResolver = TestDescriptorResolverRegistry.getTestDescriptorResolver(
+				secondaryEngineSegments.get(0).getValue());
+		if (secondaryTestDescriptorResolver == null) {
+			// TODO log error
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		// TODO make existing testDescriptors work if they are only secondary resolvers (maybe they do already)
+		Optional<String> secondaryUniformPath = secondaryTestDescriptorResolver.getUniformPath(testDescriptor);
+		if (!secondaryUniformPath.isPresent()) {
+			// TODO log error
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		LOGGER.info(suiteSegment.getValue() + "/" + secondaryUniformPath.get());
+		return Optional.of(suiteSegment.getValue() + "/" +
+				secondaryUniformPath.get());
 	}
 
 	@Override
 	public Optional<String> getClusterId(TestDescriptor testDescriptor) {
 		// Delegate to nested engines via the TestDescriptorResolverRegistry like this:
 		// return Optional.of(TestDescriptorResolverRegistry.getTestDescriptorResolver(engineID).getUniformPath());
-		return Optional.empty();
+		List<UniqueId.Segment> segments = testDescriptor.getUniqueId().getSegments();
+		// TODO refactor stings to constants (or reuse the ones we already have)
+		if (segments.size() < 3 || !segments.get(0).getType().equals("engine") || !segments.get(1).getType()
+				.equals("suite") || !segments.get(2).getType().equals("engine")) {
+			// TODO Do we actually want to use this default or do we want to crash?
+			LOGGER.severe(() ->
+					"Assuming structure [engine:junit-platform-suite]/[suite:mySuite]/[engine:anotherEngine] " +
+							"for junit-platform-suite tests. Using "
+							+ testDescriptor.getUniqueId()
+							.toString() + "as uniform path as fallback.");
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		UniqueId.Segment suiteSegment = segments.get(1);
+		List<UniqueId.Segment> secondaryEngineSegments = segments.subList(2, segments.size());
+
+		if (secondaryEngineSegments.isEmpty() || !secondaryEngineSegments.get(0).getType().equals("engine")) {
+			// TODO log error
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		ITestDescriptorResolver secondaryTestDescriptorResolver = TestDescriptorResolverRegistry.getTestDescriptorResolver(
+				secondaryEngineSegments.get(0).getValue());
+		if (secondaryTestDescriptorResolver == null) {
+			// TODO log error
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+
+		Optional<String> secondaryClusterId = secondaryTestDescriptorResolver.getClusterId(testDescriptor);
+		if (!secondaryClusterId.isPresent()) {
+			// TODO log error
+			return Optional.of(testDescriptor.getUniqueId().toString());
+		}
+		return Optional.of(suiteSegment.getValue() + "/" +
+				secondaryClusterId.get());
 	}
 
 	@Override
