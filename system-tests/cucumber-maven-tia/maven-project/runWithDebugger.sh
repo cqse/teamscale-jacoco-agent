76da9a81cdb4bfda92b667fc678d1ac5fcb7b4a0
Thomas Pettinger <pettinger@cqse.eu>
2023-08-30 16:50:52 +0200

    TS-34497 Add small script to debug the impacted test engine inside the forked surefire process

---
+++ system-tests/cucumber-maven-tia/maven-project/runWithDebugger.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Call this script to debug the impacted test engine (running within the forked JVM process of surefire).
+# Use an IntelliJ remote run config and connect to the default JVM debugging port 5005
+# Adapt the AGENT_VERSION to the one currently set in the `build.gradle.kts` in the root of this repo.
+# TEAMSCALE_PORT can stay as is (just needs to be set at all).
+AGENT_VERSION="31.0.0-cucumber-SNAPSHOT" TEAMSCALE_PORT=63800 ./mvnw -Dmaven.surefire.debug clean verify
