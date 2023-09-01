de495a362d723d7d98dbdf0521ba2601c629020c
Thomas Pettinger <pettinger@cqse.eu>
2023-09-01 16:14:34 +0200

    TS-34497 Change agent version in runWithDebugger.sh

--- system-tests/cucumber-maven-tia/maven-project/runWithDebugger.sh
+++ system-tests/cucumber-maven-tia/maven-project/runWithDebugger.sh
@@ -3,4 +3,4 @@
 # Use an IntelliJ remote run config and connect to the default JVM debugging port 5005
 # Adapt the AGENT_VERSION to the one currently set in the `build.gradle.kts` in the root of this repo.
 # TEAMSCALE_PORT can stay as is (just needs to be set at all).
-AGENT_VERSION="31.0.0-cucumber-SNAPSHOT" TEAMSCALE_PORT=63800 ./mvnw -Dmaven.surefire.debug clean verify
+AGENT_VERSION="32.1.0" TEAMSCALE_PORT=63800 ./mvnw -Dmaven.surefire.debug clean verify
