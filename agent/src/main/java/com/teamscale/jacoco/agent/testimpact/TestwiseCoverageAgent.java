45a706d000180225d5309c04d9610cfbaf4b5212
unknown <jveihelmann@cqse.eu>
2023-09-15 11:13:30 +0200

    TS-29012 reset file

--- agent/src/main/java/com/teamscale/jacoco/agent/testimpact/TestwiseCoverageAgent.java
+++ agent/src/main/java/com/teamscale/jacoco/agent/testimpact/TestwiseCoverageAgent.java
@@ -5,21 +5,18 @@
 +-------------------------------------------------------------------------*/
 package com.teamscale.jacoco.agent.testimpact;
 
-import java.io.IOException;
-
+import com.teamscale.jacoco.agent.AgentBase;
+import com.teamscale.jacoco.agent.options.AgentOptions;
+import com.teamscale.jacoco.agent.util.LoggingUtils;
+import com.teamscale.report.testwise.jacoco.JaCoCoTestwiseReportGenerator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.slf4j.Logger;
 
-import com.teamscale.jacoco.agent.AgentBase;
-import com.teamscale.jacoco.agent.options.AgentOptions;
-import com.teamscale.jacoco.agent.upload.UploaderException;
-import com.teamscale.jacoco.agent.util.LoggingUtils;
-import com.teamscale.report.testwise.jacoco.JaCoCoTestwiseReportGenerator;
+import java.io.IOException;
 
 /**
- * A wrapper around the JaCoCo Java agent that starts a HTTP server and listens
- * for test events.
+ * A wrapper around the JaCoCo Java agent that starts a HTTP server and listens for test events.
  */
 public class TestwiseCoverageAgent extends AgentBase {
 
@@ -29,7 +26,7 @@ public class TestwiseCoverageAgent extends AgentBase {
 	protected final TestEventHandlerStrategyBase testEventHandler;
 
 	/** Creates a {@link TestwiseCoverageAgent} based on the given options. */
-	public static TestwiseCoverageAgent create(AgentOptions agentOptions) throws IOException, UploaderException {
+	public static TestwiseCoverageAgent create(AgentOptions agentOptions) throws IOException {
 		Logger logger = LoggingUtils.getLogger(JaCoCoTestwiseReportGenerator.class);
 		JaCoCoTestwiseReportGenerator reportGenerator = new JaCoCoTestwiseReportGenerator(
 				agentOptions.getClassDirectoriesOrZips(), agentOptions.getLocationIncludeFilter(),
@@ -39,22 +36,23 @@ public class TestwiseCoverageAgent extends AgentBase {
 				reportGenerator);
 	}
 
+
 	public TestwiseCoverageAgent(AgentOptions options, TestExecutionWriter testExecutionWriter,
-			JaCoCoTestwiseReportGenerator reportGenerator) throws IllegalStateException, UploaderException {
+								 JaCoCoTestwiseReportGenerator reportGenerator) throws IllegalStateException {
 		super(options);
 		switch (options.getTestwiseCoverageMode()) {
-		case TEAMSCALE_UPLOAD:
-			testEventHandler = new CoverageToTeamscaleStrategy(controller, options, reportGenerator);
-			break;
-		case DISK:
-			testEventHandler = new CoverageToDiskStrategy(controller, options, reportGenerator);
-			break;
-		case HTTP:
-			testEventHandler = new CoverageViaHttpStrategy(controller, options, reportGenerator);
-			break;
-		default:
-			testEventHandler = new CoverageToExecFileStrategy(controller, options, testExecutionWriter);
-			break;
+			case TEAMSCALE_UPLOAD:
+				testEventHandler = new CoverageToTeamscaleStrategy(controller, options, reportGenerator);
+				break;
+			case DISK:
+				testEventHandler = new CoverageToDiskStrategy(controller, options, reportGenerator);
+				break;
+			case HTTP:
+				testEventHandler = new CoverageViaHttpStrategy(controller, options, reportGenerator);
+				break;
+			default:
+				testEventHandler = new CoverageToExecFileStrategy(controller, options, testExecutionWriter);
+				break;
 		}
 
 	}
