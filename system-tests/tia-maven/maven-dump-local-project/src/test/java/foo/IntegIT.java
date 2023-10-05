3f40d2a9ba968aa42fb4b3f2a7e042faccdb8d7e
unknown <jveihelmann@cqse.eu>
2023-10-05 17:03:52 +0200

    TS-35378 clean only once before parallel test execution

--- system-tests/tia-maven/maven-dump-local-project/src/test/java/foo/IntegIT.java
+++ system-tests/tia-maven/maven-dump-local-project/src/test/java/foo/IntegIT.java
@@ -9,12 +9,14 @@ public class IntegIT {
 	@Test
 	public void itBla() throws Exception {
 		new SUT().bla();
+		// Add sleep so we can simulate test execution and test parallel test execution
 		Thread.sleep(2000);
 	}
 
 	@Test
 	public void itFoo() throws Exception {
 		new SUT().foo();
+		// Add sleep so we can simulate test execution and test parallel test execution
 		Thread.sleep(2000);
 	}
 }
--- system-tests/tia-maven/src/test/java/com/teamscale/tia/TiaMavenMultipleJobsTest.java
+++ system-tests/tia-maven/src/test/java/com/teamscale/tia/TiaMavenMultipleJobsTest.java
@@ -1,24 +1,23 @@
 package com.teamscale.tia;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.File;
-import java.nio.file.Paths;
-
 import org.conqat.lib.commons.filesystem.FileSystemUtils;
 import org.conqat.lib.commons.io.ProcessUtils;
 import org.conqat.lib.commons.system.SystemUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
- * Test class to check if multiple maven plugins can be started with dynamic
- * port allocation.
+ * Test class to check if multiple maven plugins can be started with dynamic port allocation.
  */
 public class TiaMavenMultipleJobsTest {
 
 	/**
-	 * Starts multiple maven processes and checks that the ports are dynamically set
-	 * and the servers are correctly started.
+	 * Starts multiple maven processes and checks that the ports are dynamically set and the servers are correctly
+	 * started.
 	 */
 	@Test
 	public void testMavenTia() throws Exception {
@@ -27,11 +26,16 @@ public class TiaMavenMultipleJobsTest {
 		if (SystemUtils.isWindows()) {
 			executable = Paths.get("maven-dump-local-project", "mvnw.cmd").toUri().getPath();
 		}
-		for (int i = 0; i < 3; i++) {
-			new ProcessBuilder(executable, "clean", "verify").directory(workingDirectory).start();
-		}
+		// Clean once before testing parallel execution and make sure that the cleaning
+		// process is finished before testing.
 		ProcessUtils.ExecutionResult result = ProcessUtils
-				.execute(new ProcessBuilder(executable, "clean", "verify").directory(workingDirectory));
+				.execute(new ProcessBuilder(executable, "clean").directory(workingDirectory));
+		assertThat(result.terminatedByTimeoutOrInterruption()).isFalse();
+		for (int i = 0; i < 3; i++) {
+			new ProcessBuilder(executable, "verify").directory(workingDirectory).start();
+		}
+		result = ProcessUtils
+				.execute(new ProcessBuilder(executable, "verify").directory(workingDirectory));
 		// Get additional output for error debugging.
 		System.out.println(result.getStdout());
 		File configFile = new File(Paths.get(workingDirectory.getAbsolutePath(), "target", "tia", "agent.log").toUri());
