e83982291a4bb75eac5a52dedf0c6eb188ee8d35
unknown <jveihelmann@cqse.eu>
2023-10-05 08:50:05 +0200

    TS-35378 add missing import

--- teamscale-maven-plugin/src/main/java/com/teamscale/tia/maven/TiaMojoBase.java
+++ teamscale-maven-plugin/src/main/java/com/teamscale/tia/maven/TiaMojoBase.java
@@ -18,6 +18,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
