9b7b93889537f598c1ad5c11e1e4dcc26d981462
Fabian Streitel <streitel@cqse.eu>
2023-11-03 08:31:35 +0100

    log to /tmp by default

--- agent/src/main/java/com/teamscale/jacoco/agent/util/AgentUtils.java
+++ agent/src/main/java/com/teamscale/jacoco/agent/util/AgentUtils.java
@@ -2,13 +2,51 @@ package com.teamscale.jacoco.agent.util;
 
 import com.teamscale.jacoco.agent.PreMain;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /** General utilities for working with the agent. */
 public class AgentUtils {
+
+	private static Path mainTempDirectory = null;
+
+	/**
+	 * Returns a string that <i>probably</i> contains the PID.
+	 * <p>
+	 * On Java 9 there is an API to get the PID. But since we support Java 8, we may fall back to an undocumented API
+	 * that at least contains the PID in most JVMs.
+	 * <p>
+	 * See <a href="https://stackoverflow.com/questions/35842/how-can-a-java-program-get-its-own-process-id">This
+	 * StackOverflow question</a>
+	 */
+	public static String getPID() {
+		try {
+			Class<?> processHandleClass = Class.forName("java.lang.ProcessHandle");
+			Object processHandle = processHandleClass.getMethod("current").invoke(null);
+			Long pid = (Long) processHandleClass.getMethod("pid").invoke(processHandle);
+			return pid.toString();
+		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException |
+				 InvocationTargetException e) {
+			return ManagementFactory.getRuntimeMXBean().getName();
+		}
+	}
+
+	public static Path getMainTempDirectory() {
+		if (mainTempDirectory == null) {
+			try {
+				mainTempDirectory = Files.createTempDirectory("teamscale-java-profiler-" + getPID());
+			} catch (IOException e) {
+				throw new RuntimeException("Failed to create temporary directory for agent files", e);
+			}
+		}
+		return mainTempDirectory;
+	}
 
 	/** Returns the directory that contains the agent or null if it can't be resolved. */
 	public static Path getAgentDirectory() {
--- agent/src/main/java/com/teamscale/jacoco/agent/util/LogDirectoryPropertyDefiner.java
+++ agent/src/main/java/com/teamscale/jacoco/agent/util/LogDirectoryPropertyDefiner.java
@@ -3,18 +3,12 @@ package com.teamscale.jacoco.agent.util;
 import ch.qos.logback.core.PropertyDefinerBase;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /** Defines a property that contains the default path to which log files should be written. */
 public class LogDirectoryPropertyDefiner extends PropertyDefinerBase {
 	@Override
 	public String getPropertyValue() {
-		Path agentDirectory = AgentUtils.getAgentDirectory();
-		if (agentDirectory == null) {
-			// we can't log the exception yet since logging is not yet initialized
-			// fall back to the working directory
-			return Paths.get(".").toAbsolutePath().toString();
-		}
-		return agentDirectory.resolve("logs").toAbsolutePath().toString();
+		Path tempDirectory = AgentUtils.getMainTempDirectory();
+		return tempDirectory.resolve("logs").toAbsolutePath().toString();
 	}
 }
