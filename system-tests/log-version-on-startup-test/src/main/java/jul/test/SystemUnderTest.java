345a8331732f2ada941efd6154ce0938c0343a2d
Fabian Streitel <streitel@cqse.eu>
2023-11-30 13:37:01 +0100

    remove unncessary test classes

--- system-tests/log-version-on-startup-test/src/main/java/jul/test/SystemUnderTest.java
+++ system-tests/log-version-on-startup-test/src/main/java/jul/test/SystemUnderTest.java
@@ -5,16 +5,7 @@ package jul.test;
  */
 public class SystemUnderTest {
 
-	public static boolean logManagerInitialized = false;
-
 	public static void main(String[] args) {
-		if (logManagerInitialized) {
-			throw new RuntimeException("Expected the LogManager to not be initialized by the agent. " +
-					"This is required to make the agent work in certain contexts like JBoss Wildfly. " +
-					"It wants to set it's own LogManager, but cannot do so if our agent has already initialized it " +
-					"accidentally e.g. by calling `java.util.logging.Logger.getLogger(...)`. " +
-					"This should be called nowhere, because we transform all class files automatically to use " +
-					"Logger.global instead which works without initializing the LogManager.");
-		}
+		// doesn't need to do anything for this test
 	}
 }
