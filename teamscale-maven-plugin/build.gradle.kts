694d12bc3f26af42808d71ad5abe1d0cae46d4e1
Stefan Brand <brand@cqse.eu>
2023-12-20 13:55:53 +0100

    Try to fix Maven Publishing: set revision explicitly, remove closeSonatypeStagingRepository task, also depend on teamscale-client

--- teamscale-maven-plugin/build.gradle.kts
+++ teamscale-maven-plugin/build.gradle.kts
@@ -10,6 +10,9 @@ abstract class MavenExec : Exec() {
 			project.file("pom.xml").readText().replace(
 				Regex("<teamscale.agent.version>[^<]+</teamscale.agent.version>"),
 				"<teamscale.agent.version>${project.version}</teamscale.agent.version>"
+			).replace(
+				Regex("<revision>[^<]+</revision>"),
+				"<revision>${project.version}</revision>"
 			)
 		)
 		super.exec()
@@ -17,6 +20,9 @@ abstract class MavenExec : Exec() {
 			project.file("pom.xml").readText().replace(
 				Regex("<teamscale.agent.version>[^<]+</teamscale.agent.version>"),
 				"<teamscale.agent.version>29.0.0</teamscale.agent.version>"
+			).replace(
+				Regex("<revision>[^<]+</revision>"),
+				"<revision>1.0.0-SNAPSHOT</revision>"
 			)
 		)
 	}
