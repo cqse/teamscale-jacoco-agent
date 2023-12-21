74a2cd91c4344c9254eae84e785b32b3e957c8af
Fabian Streitel <streitel@cqse.eu>
2023-12-21 16:25:30 +0100

    try fixing tests

--- installer/src/test/java/com/teamscale/profiler/installer/utils/TestUtils.java
+++ installer/src/test/java/com/teamscale/profiler/installer/utils/TestUtils.java
@@ -10,7 +10,10 @@ import java.nio.file.attribute.AclEntryFlag;
 import java.nio.file.attribute.AclEntryPermission;
 import java.nio.file.attribute.AclEntryType;
 import java.nio.file.attribute.AclFileAttributeView;
+import java.nio.file.attribute.DosFileAttributeView;
+import java.nio.file.attribute.UserPrincipal;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,22 +21,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 /** Generic utilities for tests. */
 public class TestUtils {
 
+	private static final EnumSet<AclEntryPermission> READ_ONLY_ACL = EnumSet.of(AclEntryPermission.WRITE_DATA,
+			AclEntryPermission.DELETE, AclEntryPermission.DELETE_CHILD, AclEntryPermission.ADD_FILE,
+			AclEntryPermission.ADD_SUBDIRECTORY, AclEntryPermission.APPEND_DATA, AclEntryPermission.WRITE_ATTRIBUTES);
+
 	/** Changes the given path to read-only and asserts that the change was successful. */
 	public static void makePathReadOnly(Path path) throws IOException {
 		if (SystemUtils.IS_OS_WINDOWS) {
-			AclFileAttributeView view = Files.getFileAttributeView(path, AclFileAttributeView.class);
+			// File#setWritable doesn't work under Windows 11 (always returns false).
+			// The following procedure was derived from https://stackoverflow.com/a/25747561/1396068
+			// and verified under Windows 11. Only marks exactly this file/folder as read only. Any existing files
+			// under this path are still deletable.
+			DosFileAttributeView dosAttributes = Files.getFileAttributeView(path, DosFileAttributeView.class);
+			dosAttributes.setReadOnly(true);
 
-			List<AclEntry> newAcl = new ArrayList<>(view.getAcl());
-			newAcl.add(AclEntry.newBuilder()
+			AclFileAttributeView aclView = Files.getFileAttributeView(path, AclFileAttributeView.class);
+			UserPrincipal owner = aclView.getOwner();
+			List<AclEntry> newAcl = new ArrayList<>(aclView.getAcl());
+			// deny entries must come before allow entries
+			newAcl.add(0, AclEntry.newBuilder()
 					.setType(AclEntryType.DENY)
-					.setPrincipal(view.getOwner())
-					.setPermissions(AclEntryPermission.WRITE_DATA, AclEntryPermission.DELETE,
-							AclEntryPermission.DELETE_CHILD,
-							AclEntryPermission.ADD_FILE, AclEntryPermission.ADD_SUBDIRECTORY,
-							AclEntryPermission.APPEND_DATA)
-					.setFlags(AclEntryFlag.DIRECTORY_INHERIT, AclEntryFlag.FILE_INHERIT)
+					.setPrincipal(owner)
+					.setPermissions(READ_ONLY_ACL)
+					.setFlags(AclEntryFlag.FILE_INHERIT, AclEntryFlag.DIRECTORY_INHERIT)
 					.build());
-			view.setAcl(newAcl);
+			aclView.setAcl(newAcl);
 		} else {
 			assertThat(path.toFile().setWritable(false, false))
 					.withFailMessage("Failed to mark " + path + " as writable = " + false).isTrue();
