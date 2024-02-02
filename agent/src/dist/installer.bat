10516c826f03e57996d4b9e885fa2a057477a8b3
Fabian Streitel <streitel@cqse.eu>
2024-02-02 10:22:11 +0100

    make sure the profiler is not profiling the uninstaller
    this may cause problems when we try to delete files etc.

--- agent/src/dist/installer.bat
+++ agent/src/dist/installer.bat
@@ -2,4 +2,8 @@
 set DIR="%~dp0"
 set BAT_EXEC="%DIR:"=%\installer\installer-windows-x86_64\bin\installer.bat"
 
+rem ensure that we don't accidentally profile the uninstaller
+set JAVA_TOOL_OPTIONS=
+set _JAVA_OPTIONS=
+
 pushd %DIR% & %BAT_EXEC% %* & popd
--- agent/src/dist/installer.sh
+++ agent/src/dist/installer.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 DIR="${0%/*}"
+
+# ensure that we don't accidentally profile the uninstaller
+set JAVA_TOOL_OPTIONS=
+set _JAVA_OPTIONS=
+
 exec "$DIR/installer/installer-linux-x86_64/bin/installer" "$@"
