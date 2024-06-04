47d18bb55ec9ad0fe8da70ab9fc10632d02c6761
Florian Dreier <dreier@cqse.eu>
2024-06-04 11:19:49 +0200

    TS-38982 Added a test that the tests are still executed with includes specified

--- system-tests/gradle-multi-module/gradle-project/buildSrc/src/main/kotlin/com.example.java-convention.gradle.kts
+++ system-tests/gradle-multi-module/gradle-project/buildSrc/src/main/kotlin/com.example.java-convention.gradle.kts
@@ -1,49 +1,50 @@
 plugins {
-    java
-    id("com.teamscale")
+	java
+	id("com.teamscale")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation(platform("org.junit:junit-bom:5.9.1"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 teamscale {
 
-    server {
-        url = "http://localhost:${System.getenv("TEAMSCALE_PORT")}/"
-        userName = "admin"
-        userAccessToken = "mrllRGxXJrsyL0UID3gGcJnZuQT4EyWr"
-        project = "submodules"
-    }
+	server {
+		url = "http://localhost:${System.getenv("TEAMSCALE_PORT")}/"
+		userName = "admin"
+		userAccessToken = "mrllRGxXJrsyL0UID3gGcJnZuQT4EyWr"
+		project = "submodules"
+	}
 
-    report {
-        testwiseCoverage {
-            partition.set("Unit Tests")
-        }
-    }
-    commit {
-        branchName = "branch1"
-        timestamp = "123456"
-    }
+	report {
+		testwiseCoverage {
+			partition.set("Unit Tests")
+		}
+	}
+	commit {
+		branchName = "branch1"
+		timestamp = "123456"
+	}
 
 }
 
 tasks.register("tiaTests", com.teamscale.TestImpacted::class.java) {
-    useJUnitPlatform()
-    jacoco {
-    }
+	useJUnitPlatform()
+	configure<JacocoTaskExtension> {
+		includes = listOf("org.example.*")
+	}
 }
