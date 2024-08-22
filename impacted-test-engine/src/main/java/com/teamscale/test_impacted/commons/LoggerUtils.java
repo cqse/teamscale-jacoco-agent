2855dc8cad41b02668dd11c18d27336ba7ada2ec
Thomas Pettinger <pettinger@cqse.eu>
2024-08-22 14:31:39 +0200

    TS-39915 Ensure that we can make use of the java.util.logging.config.file system property

--- impacted-test-engine/src/main/java/com/teamscale/test_impacted/commons/LoggerUtils.java
+++ impacted-test-engine/src/main/java/com/teamscale/test_impacted/commons/LoggerUtils.java
@@ -1,6 +1,10 @@
 package com.teamscale.test_impacted.commons;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -12,8 +16,12 @@ import java.util.logging.SimpleFormatter;
 public class LoggerUtils {
 
 	private static final Logger MAIN_LOGGER;
+	public static final String JAVA_UTIL_LOGGING_CONFIG_FILE_SYSTEM_PROPERTY = "java.util.logging.config.file";
 
 	static {
+		// Needs to be at the very top so it also takes affect when setting the log level for Console handlers
+		useDefaultJULConfigFile();
+
 		MAIN_LOGGER = Logger.getLogger("com.teamscale");
 		MAIN_LOGGER.setUseParentHandlers(false);
 		ConsoleHandler handler = new ConsoleHandler();
@@ -24,7 +32,26 @@ public class LoggerUtils {
 				return String.format("[%1$s] %2$s%n", lr.getLevel().getLocalizedName(), lr.getMessage());
 			}
 		});
+
+
 		MAIN_LOGGER.addHandler(handler);
+	}
+
+	/**
+	 * Normally, the java util logging framework picks up the config file specified via the system property
+	 * {@value #JAVA_UTIL_LOGGING_CONFIG_FILE_SYSTEM_PROPERTY}. For some reason, this does not work here, so we need to
+	 * teach the log manager to use it.
+	 */
+	private static void useDefaultJULConfigFile() {
+		String loggingPropertiesFilePath = System.getProperty(JAVA_UTIL_LOGGING_CONFIG_FILE_SYSTEM_PROPERTY);
+		if (loggingPropertiesFilePath != null) {
+			File loggingPropertiesFile = new File(loggingPropertiesFilePath);
+			try {
+				LogManager.getLogManager().readConfiguration(Files.newInputStream(loggingPropertiesFile.toPath()));
+			} catch (IOException e) {
+				// Ignore, we cant load the logging config so we just use the defaults
+			}
+		}
 	}
 
 	/**
