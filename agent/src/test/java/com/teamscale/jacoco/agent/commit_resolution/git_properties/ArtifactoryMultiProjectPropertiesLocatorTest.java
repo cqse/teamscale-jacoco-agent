0f646a0e7ae3317257e9a32f52dd5e1652a2640f
Max Schallermayer <schallermayer@cqse.eu>
2024-09-03 17:14:07 +0200

    TS-37750 Add test to see if locator.searchFile adds Artifactory uploaders correctly

--- agent/src/test/java/com/teamscale/jacoco/agent/commit_resolution/git_properties/ArtifactoryMultiProjectPropertiesLocatorTest.java
+++ agent/src/test/java/com/teamscale/jacoco/agent/commit_resolution/git_properties/ArtifactoryMultiProjectPropertiesLocatorTest.java
@@ -4,23 +4,17 @@ import com.teamscale.jacoco.agent.upload.artifactory.ArtifactoryMultiUploader;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ArtifactoryMultiProjectPropertiesLocatorTest {
 
-	private static final String TEST_URL = "https://www.wikipedia.org/";
-
 	@Test
 	public void findsUrlProperty(){
-		List<String> urls = new ArrayList<>();
 		ArtifactoryMultiUploader uploader = new ArtifactoryMultiUploader();
 		ArtifactoryMultiProjectPropertiesLocator locator = new ArtifactoryMultiProjectPropertiesLocator(uploader, true);
 		File file = new File(getClass().getResource("artifactory-properties").getFile());
 		locator.searchFileForGitPropertiesAsync(file, false);
-		//assertThat(uploader.getArtifactoryUploaders().get(0).describe()).isEqualTo("Uploading to " + TEST_URL);
-		// we use executor so concurrency! cannot simply step through
+		assertThat(uploader.getArtifactoryUploaders().size()).isEqualTo(2);
 	}
 }
