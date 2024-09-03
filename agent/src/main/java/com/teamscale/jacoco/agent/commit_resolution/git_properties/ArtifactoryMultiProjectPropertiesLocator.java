c086f0ec1d81d20ba01b71d51ce4d2ace9713a39
Max Schallermayer <schallermayer@cqse.eu>
2024-09-03 17:13:18 +0200

    TS-37750 Make ArtifactoryMultiProjectPropertiesLocator.searchFileForGitPropertiesAsync not async and remove executor for understanding

--- agent/src/main/java/com/teamscale/jacoco/agent/commit_resolution/git_properties/ArtifactoryMultiProjectPropertiesLocator.java
+++ agent/src/main/java/com/teamscale/jacoco/agent/commit_resolution/git_properties/ArtifactoryMultiProjectPropertiesLocator.java
@@ -1,5 +1,6 @@
 package com.teamscale.jacoco.agent.commit_resolution.git_properties;
 
+import com.teamscale.client.EReportFormat;
 import com.teamscale.jacoco.agent.upload.artifactory.ArtifactoryConfig;
 import com.teamscale.jacoco.agent.upload.artifactory.ArtifactoryMultiUploader;
 import com.teamscale.jacoco.agent.upload.artifactory.ArtifactoryUploader;
@@ -17,52 +18,31 @@ import java.util.concurrent.Executors;
 public class ArtifactoryMultiProjectPropertiesLocator implements IGitPropertiesLocator {
 
 	private final Logger logger = LoggingUtils.getLogger(ArtifactoryMultiProjectPropertiesLocator.class);
-	private final Executor executor;
 	private final ArtifactoryMultiUploader uploader;
 	private final boolean recursiveSearch;
 
-	public ArtifactoryMultiProjectPropertiesLocator(ArtifactoryMultiUploader uploader, Executor executor,
+	public ArtifactoryMultiProjectPropertiesLocator(ArtifactoryMultiUploader uploader,
 			boolean recursiveSearch) {
-		this.executor = executor;
 		this.uploader = uploader;
 		this.recursiveSearch = recursiveSearch;
 	}
 
-	public ArtifactoryMultiProjectPropertiesLocator(ArtifactoryMultiUploader uploader, boolean recursiveSearch) {
-		// using a single threaded executor allows this class to be lock-free
-		this(uploader, Executors
-				.newSingleThreadExecutor(
-						new DaemonThreadFactory(GitMultiProjectPropertiesLocator.class,
-								"git.properties Jar scanner thread")), recursiveSearch);
-	}
-
-	/**
-	 * Asynchronously searches the given jar file for git.properties files and adds a corresponding uploader to the
-	 * multi-project uploader.
-	 */
+	// TODO why should this be async?
 	@Override
 	public void searchFileForGitPropertiesAsync(File file, boolean isJarFile) {
-		executor.execute(() -> searchFile(file, isJarFile));
-	}
-
-	void searchFile(File file, boolean isJarFile) {
 		logger.debug("Searching file {} for multiple git.properties", file.toString());
 		try {
 			List<String> urls = GitPropertiesLocatorUtils.getAllArtifactoryUrlsFromGitProperties(
 					file, isJarFile, recursiveSearch);
 			for (String url : urls) {
 				ArtifactoryConfig artifactoryConfig = new ArtifactoryConfig();
-				//artifactoryConfig.url = HttpUrl.parse(url);
+				artifactoryConfig.url = HttpUrl.parse(url);
 				// TODO add username and password where do i get this?
 				// TODO instead of null parse actual parameters, check where ArtifactoryUploader constructor is called and what is passed there
-				uploader.addArtifactoryUploader(new ArtifactoryUploader(artifactoryConfig, null, null));
-				System.out.println("moin");
+				uploader.addArtifactoryUploader(new ArtifactoryUploader(artifactoryConfig, null, EReportFormat.JACOCO));
 			}
-
 		} catch (IOException e) {
 			logger.error("Error trying to find artifactory urls in directory {}", file, e);
 		}
-
-
 	}
 }
