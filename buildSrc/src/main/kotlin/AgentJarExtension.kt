fb062666a580b9269fe3e94379b83f7266a2cc91
Max Schallermayer <schallermayer@cqse.eu>
2024-09-13 14:55:03 +0200

    TS-37750 Fix hard-coded path to jar

--- buildSrc/src/main/kotlin/AgentJarExtension.kt
+++ buildSrc/src/main/kotlin/AgentJarExtension.kt
@@ -6,7 +6,7 @@ import java.io.File
 
 /** Determines the path under which the com.teamscale.agent-jar plugin stored the agent jar. */
 val Task.agentJar: File
-	get() = File("/home/max_schaller/.m2/repository/com/teamscale/teamscale-jacoco-agent/34.0.1-SNAPSHOT/teamscale-jacoco-agent-34.0.1-SNAPSHOT.jar")
+	get() = extra["agentJar"] as File
 
 /** Adds a convenient way to attach the Teamscale JaCoCo agent to the JVM with the given options in a readable map format. */
 fun JavaExec.teamscaleAgent(options: Map<String, String>) {
