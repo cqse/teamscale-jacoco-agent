5004ab35729fc60e571d711d2f0747b39fb412f9
Constructor <fractalminds@protonmail.com>
2024-10-31 01:06:05 +0100

    TS-38628 Extension function to enable default implementation for logger

--- report-generator/src/main/kotlin/com/teamscale/report/util/ILogger.kt
+++ report-generator/src/main/kotlin/com/teamscale/report/util/ILogger.kt
@@ -3,30 +3,30 @@ package com.teamscale.report.util
 /**
  * Minimal logging interface.
  *
- *
  * We use this to work around some strange problems when using log4j from the Teamscale Gradle plugin.
  */
 interface ILogger {
-	/** Logs at debug level.  */
-	fun debug(message: String?)
 
-	/** Logs at info level.  */
-	fun info(message: String?)
+	/** Logs at debug level. */
+	fun debug(message: String)
 
-	/** Logs at warning level.  */
-	fun warn(message: String?)
+	/** Logs at info level. */
+	fun info(message: String)
 
-	/** Logs at warning level. The given [Throwable] may be null.  */
-	fun warn(message: String?, throwable: Throwable?)
+	/** Logs at warning level. */
+	fun warn(message: String)
 
-	/** Logs at error level.  */
+	/** Logs at warning level. The given [Throwable] may be null. */
+	fun warn(message: String, throwable: Throwable?)
+
+	/** Logs at error level. */
 	fun error(throwable: Throwable)
 
-	/** Logs at error level. The given [Throwable] may be null.  */
-	fun error(message: String?, throwable: Throwable? = null)
+	/** Logs at error level. The given [Throwable] may be null. */
+	fun error(message: String, throwable: Throwable?)
+}
 
-	/** Logs at error level.  */
-	fun error(message: String?) {
-		error(message, null)
-	}
+/** Default implementation for `error(String message)` method. */
+fun ILogger.error(message: String) {
+	error(message, null)
 }
