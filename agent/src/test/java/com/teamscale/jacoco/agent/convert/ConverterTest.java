39e37d4bc8ed319ed78809582f4a45e62bc81b9b
Zsofia Borbely <borbely@cqse.eu>
2025-02-25 14:23:15 +0100

    TS-41980 Add test

--- agent/src/test/java/com/teamscale/jacoco/agent/convert/ConverterTest.java
+++ agent/src/test/java/com/teamscale/jacoco/agent/convert/ConverterTest.java
@@ -36,6 +36,24 @@ public class ConverterTest {
 		assertThat(xml).isNotEmpty().contains("<package").contains("<sourcefile").contains("<counter").contains("TestClass");
 	}
 
+	@Test
+	public void testNestedJar(@TempDir File tempDir) throws Exception {
+		File execFile = new File(getClass().getResource("coverage.exec").toURI());
+		File classFile = new File(getClass().getResource("TestClass.jar.zip").toURI());
+		File outputFile = new File(tempDir, "coverage.xml");
+
+		ConvertCommand arguments = new ConvertCommand();
+		arguments.inputFiles = Collections.singletonList(execFile.getAbsolutePath());
+		arguments.outputFile = outputFile.getAbsolutePath();
+		arguments.classDirectoriesOrZips = Collections.singletonList(classFile.getAbsolutePath());
+
+		new Converter(arguments).runJaCoCoReportGeneration();
+
+		String xml = FileSystemUtils.readFileUTF8(outputFile);
+		System.err.println(xml);
+		assertThat(xml).isNotEmpty().contains("<package").contains("<sourcefile").contains("<counter").contains("TestClass");
+	}
+
 	/**
 	 * Ensures that running the converter on valid input does not yield any errors and produces a coverage XML report.
 	 */
